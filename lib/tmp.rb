35b81e07ab5cf27f576032cf1315479a99316a36
abragin <abragin@gmail.com>
2010-08-06 17:49:21 +0400

    fix

--- lib/tmp.rb
+++ lib/tmp.rb
@@ -56,7 +56,7 @@ module Tmp
 
 
   def self.fill_in_morpher_fields(klass, first_id = 0)
-    klass.all(:conditions => ['((proper_to = "") or (proper_to is NULL)) and (morpher_to is NULL or morpher_to = "") and (name_ru != "") and (name_ru is not NULL) and (id >= ?)', first_id], :order_by => :id).each do |c|
+    klass.all(:conditions => ['((proper_to = "") or (proper_to is NULL)) and (morpher_to is NULL or morpher_to = "") and (name_ru != "") and (name_ru is not NULL) and (id >= ?)', first_id]).each do |c|
       c.save_guessed
     end
   end
