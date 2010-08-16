290c74f268c7bd0cee7fe1fee510957d9f0fffee
Maxim Zakharov <mszakharov@gmail.com>
2010-08-16 21:05:37 +0400

    Предлоги в списке предложений

--- app/helpers/pricer_helper.rb
+++ app/helpers/pricer_helper.rb
@@ -44,22 +44,13 @@ module PricerHelper
     I18n.l(Date.strptime(date, '%d%m%y'), :format => '%e %B')
   end
   
-  def human_enumeration items
-    items.length == 1 ? items.first : (items[0..-2].join(', ') + ' и ' + items.last)
-  end
-  
   def human_layovers_count count
     numbers = ['одной', 'двумя', 'тремя', 'четыремя', 'пятью']
     count == 1 ? 'пересадкой' : (numbers[count - 1] + ' пересадками —')
   end
   
   def layovers_in flights
-    cities = []
-    flights.each_with_index do |flight, i|
-      city = flight.arrival.city.case_in
-      cities << (i == 0 ? city.sub(/ /, '&nbsp;') : city.split(' ')[1])
-    end
-    human_enumeration(cities)
+    flights.map {|flight| flight.arrival.city.case_in }.to_sentence
   end
   
   def segments_departure variant
@@ -82,7 +73,7 @@ module PricerHelper
       stimes.delete(excluded_variant.segments[i].dept_time) if excluded_variant
       if stimes.length > 0
         time_links = stimes.uniq.sort.map {|t| "<a href=\"#\" data-segment=\"#{i}\"><u>#{t}</u></a>" }
-        result << "<strong>#{segment['city'].case_from}</strong> — в #{human_enumeration(time_links)}"
+        result << "<strong>#{segment['city'].case_from}</strong> — в #{time_links.to_sentence(:last_word_connector => ' и в ')}"
       end
     end
     result.join(', ')
