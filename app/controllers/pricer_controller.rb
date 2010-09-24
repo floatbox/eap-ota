23a1e85016b4cb9c143ed90ded155dd60ca88943
abragin <abragin@gmail.com>
2010-09-24 16:11:34 +0400

    pricer возвращает закешированный фрагмент по параметру code + временно включил кеширование в development

--- app/controllers/pricer_controller.rb
+++ app/controllers/pricer_controller.rb
@@ -2,24 +2,32 @@ class PricerController < ApplicationController
   layout false
 
   def index
-    @search = PricerForm.new(params[:search])
-    if @search.valid?
-      @recommendations = @search.search
-      # TODO перенести в модель
-      unless @search.nonstop?
-        recommendations_nonstop = PricerForm.new( params[:search].merge(:nonstop => true)).search
-        # только новые
-        @recommendations |= recommendations_nonstop
+    if params[:code]
+      unless fragment_exist?({ :action_suffix => params[:code]})
+        render :nothing => true unless fragment_exist?({ :action_suffix => params[:code]})
+        return
       end
-      @recommendations = @recommendations.sort_by(&:price_total)
-      # все - временное
-      @cheap_recommendation = Recommendation.cheap(@recommendations)
-      @fast_recommendation = Recommendation.fast(@recommendations)
-      @optimal_recommendation = @fast_recommendation
-      @code = ShortUrl.generate_url(@recommendations.hash)
+      @code = params[:code]
+    else
+      @search = PricerForm.new(params[:search])
+      if @search.valid?
+        @recommendations = @search.search
+        # TODO перенести в модель
+        unless @search.nonstop?
+          recommendations_nonstop = PricerForm.new( params[:search].merge(:nonstop => true)).search
+          # только новые
+          @recommendations |= recommendations_nonstop
+        end
+        @recommendations = @recommendations.sort_by(&:price_total)
+        # все - временное
+        @cheap_recommendation = Recommendation.cheap(@recommendations)
+        @fast_recommendation = Recommendation.fast(@recommendations)
+        @optimal_recommendation = @fast_recommendation
+        @code = ShortUrl.generate_url(@recommendations.hash)
+      end
     end
-
-  render :partial => 'recommendations'
+    
+    render :partial => 'recommendations'
   rescue Amadeus::AmadeusError, Handsoap::Fault => e
     @error_message = e.message
     render :text => @errorMessage
