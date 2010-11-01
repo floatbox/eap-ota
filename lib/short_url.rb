6424a5303104a2c84a00265c68034f981cdc76c1
abragin <abragin@gmail.com>
2010-11-01 17:15:16 +0300

    хеши теперь используют только цифры и буквы

--- lib/short_url.rb
+++ lib/short_url.rb
@@ -1,5 +1,5 @@
 module ShortUrl
-  URL_CHARS = ('0'..'9').to_a + %w(b c d f g h j k l m n p q r s t v w x y z) + %w(B C D F G H J K L M N P Q R S T V W X Y Z - _)
+  URL_CHARS = ('0'..'9').to_a + %w(b c d f g h j k l m n p q r s t v w x y z) + %w(B C D F G H J K L M N P Q R S T V W X Y Z)
   URL_BASE = URL_CHARS.size
 
   def self.generate_url idNumber
