505d19a213c67d0876e97525bda152cfffc95c8a
codesnik <aronaxis@gmail.com>
2010-12-03 22:00:49 +0300

    братск это такая москва

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,8 @@ class HomeController < ApplicationController
   def nearest_city
     @nearest_city ||= begin
       loc = GeoIp.geolocation(request.remote_ip)
-      if loc[:latitude] == "0" && loc[:longitude] == "0"
+      if loc[:latitude] == "0" && loc[:longitude] == "0" ||
+         loc[:latitude] == "60" && loc[:longitude] == "100"
         return Location.default
       end
       City.nearest_to loc[:latitude], loc[:longitude]
