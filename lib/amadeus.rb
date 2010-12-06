e9cb6e2c9bebf3d006b618af82b46f00f8ac1f5c
codesnik <aronaxis@gmail.com>
2010-12-06 22:23:26 +0300

    неправильно сделал шорткаты

--- lib/amadeus.rb
+++ lib/amadeus.rb
@@ -6,12 +6,16 @@ module Amadeus
   def self.fake=(value); $amadeus_fake=value; end
   def self.fake; $amadeus_fake; end
 
-  def booking
-    Amadeus::Service.new(:book => true, :office => Amadeus::Session::BOOKING)
+  module Shortcuts
+    def booking
+      Amadeus::Service.new(:book => true, :office => Amadeus::Session::BOOKING)
+    end
+
+    def ticketing
+      Amadeus::Service.new(:book => true, :office => Amadeus::Session::TICKETING)
+    end
   end
 
-  def ticketing
-    Amadeus::Service.new(:book => true, :office => Amadeus::Session::TICKETING)
-  end
+  extend Shortcuts
 
 end
