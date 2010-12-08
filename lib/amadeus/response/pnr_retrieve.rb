2511c8fb5591c417bfdfc6b902db293a03870e3a
codesnik <aronaxis@gmail.com>
2010-12-08 04:04:18 +0300

    исключение не AIR сегментов из PNR

--- lib/amadeus/response/pnr_retrieve.rb
+++ lib/amadeus/response/pnr_retrieve.rb
@@ -3,7 +3,7 @@ module Amadeus
     class PNRRetrieve < Amadeus::Response::Base
 
       def flights
-        xpath("//r:itineraryInfo").map do |fi|
+        xpath("//r:itineraryInfo[r:segmentName='AIR']").map do |fi|
             Flight.new(
               :operating_carrier_iata => fi.xpath("r:travelProduct/r:companyDetail/r:identification").to_s,
               :departure_iata =>         fi.xpath("r:travelProduct/r:boardpointDetail/r:cityCode").to_s,
@@ -44,31 +44,9 @@ module Amadeus
         $1
       end
 
-      # не используется. но может пригодится еще
-      def prices
-        code_to_text = {'712' => 'Total fare amount', 'B' => 'Base fare', 'E' => 'Equivalent fare'}
-        xpath('//r:fareDataSupInformation').map do |price|
-          OpenStruct.new(
-            :fareDataQualifier => code_to_text[price.xpath("r:fareDataQualifier").to_s],
-            :fareAmount => price.xpath("r:fareAmount").to_s,
-            :fareCurrency => price.xpath("r:fareCurrency").to_s
-          )
-        end
-      end
-
-      # не используется. но может пригодится еще
-      def taxes
-        xpath('//r:taxInformation').map do |tax|
-          OpenStruct.new(
-            :taxQualifier => tax.xpath("r:taxDetails/r:taxQualifier").to_s,
-            :taxIdentifier => tax.xpath("r:taxDetails/r:taxIdentification/r:taxIdentifier").to_s,
-            :isoCountry => tax.xpath("r:taxDetails/r:taxType/r:isoCountry").to_s,
-            :taxNature => tax.xpath("r:taxDetails/r:taxNature").to_s,
-            :fareAmount => tax.xpath("r:amountDetails/r:fareDataMainInformation/r:fareAmount").to_s,
-            :fareCurrency => tax.xpath("r:amountDetails/r:fareDataMainInformation/r:fareCurrency").to_s
-          )
-        end
-      end
+      # def prices
+        # можно вытащить аналогично fare_price_pnr_with_booking_class.rb
+      # end
 
     end
   end
