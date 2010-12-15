6773d7777aec57f1f2f3e8e47b0416ae29949dd5
abragin <abragin@gmail.com>
2010-12-15 21:59:18 +0300

    ++

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,11 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  #TODO временно закрываем доступ для не залогиненых в админку
-  include Typus::Authentication
-  before_filter :require_login
+  if Rails.env.production?
+    #TODO временно закрываем доступ для не залогиненых в админку
+    include Typus::Authentication
+    before_filter :require_login
+  end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
