6ba12f3520f606e079c4f8fb0a19965d2724bab2
codesnik <aronaxis@gmail.com>
2010-12-22 06:15:58 +0300

    причесывание и логгинг для sirena/service.rb

--- lib/sirena/service.rb
+++ lib/sirena/service.rb
@@ -3,31 +3,35 @@ module Sirena
 
   class Service
 
-    Url = 'eviterra.com'#"127.0.0.1"
-    Port = 8888
-    Path = "/"
+    HOST = 'eviterra.com'#"127.0.0.1"
+    PORT = 8888
+    PATH = "/"
 
-    Parameters = {:schedule=>%w(departure arrival company date date2 time_from time_till direct)}
+    PARAMETERS = {:schedule=>%w(departure arrival company date date2 time_from time_till direct)}
 
     class << self
+
+      include FileLogger
+      def debug_dir; 'log/sirena' end
 
       def action(name, params)
         file = File.expand_path("../templates/#{name}.haml", __FILE__)
         if File.file? file
            request = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+Haml::Engine.new(File.read(file)).render(OpenStruct.new(params))
            print request+"\n\n"
-           http= Net::HTTP.new(Url, Port)
-           parse_respond(name, http.post(Path, request))
+           http= Net::HTTP.new(HOST, PORT)
+           parse_response(name, http.post(PATH, request))
         else
           puts "unknown action #{name}"
         end
       end
 
-      def parse_respond(name, http_request)
-        if http_request.code != '200'
-          puts http_request.code+": \n\n"+http_request.body
+      def parse_response(name, http_response)
+        if http_response.code != '200'
+          puts http_response.code+": \n\n"+http_response.body
         else
-          doc = Nokogiri::XML::Document.parse(http_request.body)
+          save_xml(name, http_response.body)
+          doc = Nokogiri::XML::Document.parse(http_response.body)
           error = doc.xpath('//error')
           unless error.blank?
             puts error.inner_html
