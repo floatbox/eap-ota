4b264311f69060cffb6b4d288c181048180ca05f
codesnik <aronaxis@gmail.com>
2011-01-26 08:53:43 +0300

    log/sirena.log для tail -f

--- lib/sirena/service.rb
+++ lib/sirena/service.rb
@@ -3,7 +3,7 @@ module Sirena
 
   class Service
 
-    HOST = 'eviterra.com'#"127.0.0.1"
+    HOST = 'delta.eviterra.com' #"127.0.0.1"
     PORT = 8888
     PATH = "/"
 
@@ -11,26 +11,31 @@ module Sirena
 
       include FileLogger
       def debug_dir; 'log/sirena' end
+      def debug_file; 'log/sirena.log'; end
 
       def action(name, params)
         request = Sirena::Request.for(name).new(params)
         request_body = request.render
+        log_request(name, request_body)
         response_body = do_http(request_body)
-        save_xml(name, response_body)
+        log_response(name, response_body)
         response = Sirena::Response.for(name).new(response_body)
       end
 
       # FIXME заменить нафик на Curl::Easy
       def do_http(request)
-        # sirenad понимает хедеры
-        # X-Timeout
-        # X-Priority
-        # надо бы добавить и привести в соответствие с Net::HTTP таймаутом
+        # pricing: 150
+        # client_summary: 100
+        # superclient_summary: 150
+        # остальные запросы: 40
+        # выставляю пока для прайсера, с запасом
         http = Net::HTTP.new(HOST, PORT)
-        http_response = http.post(PATH, request)
+        http.read_timeout = 160
+        http_response = http.post(PATH, request,
+                                 'X-Timeout' => '155')
 
-        if http_response.code != '200'
-          raise "HTTP error, status code: #{http_response.code}"
+        unless http_response.is_a? Net::HTTPSuccess
+          http_response.error!
         end
         http_response.body
       end
@@ -41,6 +46,22 @@ module Sirena
         end
       end
 
+      def log_request(name, request)
+        open(debug_file, 'a') {|f|
+          f.puts "==============="
+          f.puts "Request: #{name}"
+          f.puts "---------------"
+          f << request
+        }
+      end
+
+      def log_response(name, response)
+        open(debug_file, 'a') {|f|
+          f.puts "---------------"
+          f << response
+        }
+        save_xml(name, response)
+      end
     end
 
   end
