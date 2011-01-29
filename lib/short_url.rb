62be6d5c213a5ce9f6309f1033b7092260534c48
codesnik <aronaxis@gmail.com>
2011-01-29 18:22:15 +0300

    не, это невыносимо эту жавашню терпеть было

--- lib/short_url.rb
+++ lib/short_url.rb
@@ -3,12 +3,15 @@ module ShortUrl
   URL_CHARS = ('0'..'9').to_a + %w(b c d f g h j k l m n p q r s t v w x y z) + %w(B C D F G H J K L M N P Q R S T V W X Y Z)
   URL_BASE = URL_CHARS.size
 
-  def self.generate_url idNumber
-    localCount = idNumber.abs
-    result = '';
-    while (localCount != 0) && (result.length < 6)
-      rem = localCount % URL_BASE
-      localCount = (localCount - rem) / URL_BASE
+  # а можно было сделать почти то же самое вот так:
+  # id_number.to_s(36)
+
+  def self.generate_url id_number
+    local_count = id_number.abs
+    result = ''
+    while (local_count != 0) && (result.length < 6)
+      rem = local_count % URL_BASE
+      local_count = (local_count - rem) / URL_BASE
       result = URL_CHARS[rem] + result
     end
     return result
