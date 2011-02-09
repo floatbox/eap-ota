8e966213ac11bb534d782d7cd02cda7173e0bf6b
abragin <abragin@gmail.com>
2011-02-09 20:30:56 +0300

    fix правил тарифа

--- lib/amadeus/response/fare_check_rules.rb
+++ lib/amadeus/response/fare_check_rules.rb
@@ -3,7 +3,7 @@ module Amadeus
   module Response
     class FareCheckRules < Amadeus::Response::Base
       def rules
-        xpath('r:tariffInfo').map do |ti|
+        xpath('//r:tariffInfo').map do |ti|
           (ti / 'r:fareRuleText/r:freeText').map{|t| t.to_s.to_s}.join("\n")
         end
       end
