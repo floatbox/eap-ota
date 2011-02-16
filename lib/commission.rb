1ae640b12a3fe86b0586b771bfc4a232c78a05ce
codesnik <aronaxis@gmail.com>
2011-02-16 21:08:55 +0300

    SQ включен

--- lib/commission.rb
+++ lib/commission.rb
@@ -1376,36 +1376,33 @@ commission "0.5%/5"
 carrier "SQ", "SINGAPORE AIRLINES (Авиарепс)"
 ########################################
 
+example 'svosin'
 agent    "3% от опубл.тарифов (вкл.промотарифы в V и Q классах) на собств.рейсы SQ/Silk Air с началом от пунктов РФ;"
 subagent "2% от опубл.тарифов (вкл.промотарифы в V и Q классах) на собств.рейсы SQ/Silk Air с началом от пунктов РФ;"
-disabled #гео-правило
-#geo
-not_implemented
+# interline :possible
+check { country_iatas.first == 'RU'}
 commission "3%/2%"
 
+example 'svohou houmia'
+example 'housvo'
 agent    "6% от опубл.тарифов (вкл.промотарифы в V и Q классах) на собств.рейсы SQ/Silk Air с началом от пунктов РФ в/через Хьюстон (США) и от Хьюстона (США) в пункты РФ;"
 subagent "4,2% от опубл.тарифов (вкл.промотарифы в V и Q классах) на собств.рейсы SQ/Silk Air с началом от пунктов РФ в/через Хьюстон (США) и от Хьюстона (США) в пункты РФ;"
-disabled #гео-правило
-#geo
-not_implemented
+important!
+# interline :possible
+check { country_iatas.first == 'RU' && city_iatas.include?('HOU') ||
+  city_iatas.first == 'HOU' && flights.last.arrival.city.country.iata == 'RU' }
 commission "6%/4.2%"
 
+example 'miahou housvo'
+example 'sinsvo'
+example 'housvo svosin'
+example 'sinsvo svosin/su'
 agent    "3% от опубл.тарифов на собств.рейсы SQ/Silk Air с началом от пунктов, не указанных выше;"
 agent    "При продаже перевозок по Interline комиссионное вознаграждение начисляется в полном объеме, если перевозка включает хотя бы один полетный сегмент SQ/Silk Air и оформлена на бланке SQ/618. Оформление перевозки на бланках SQ/618 по маршруту, который не включает хотя бы один полетный сегмент, выполняемый SQ/Silk Air, запрещено."
 subagent "2% от опубл.тарифов на собств.рейсы SQ/Silk Air с началом от пунктов, не указанных выше;"
 subagent "При продаже перевозок по Interline комиссионное вознаграждение начисляется в полном объеме, если перевозка включает хотя бы один полетный сегмент SQ/Silk Air и оформлена на бланке SQ/618. Оформление перевозки на бланках SQ/618 по маршруту, который не включает хотя бы один полетный сегмент, выполняемый SQ/Silk Air, запрещено."
 interline :possible
-not_implemented
 commission '3%/2%'
-
-example 'mowpar parmow'
-# FIXME
-agent "временное правило, требует коррекции, если таки через хьюстон"
-subagent "временное правило, требует коррекции, если таки через хьюстон"
-commission "3%/2%"
-
-example 'mowpar parmow/ab'
-no_commission
 
 carrier "SW", "AIR NAMIBIA (АВИАРЕПС)"
 ########################################
