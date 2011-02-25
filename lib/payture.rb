16185e17eca651613c550c5ba2c9a239a94c3720
codesnik <aronaxis@gmail.com>
2011-02-25 04:17:53 +0300

    временно курс бакса - 2.91

--- lib/payture.rb
+++ lib/payture.rb
@@ -7,7 +7,7 @@ class Payture
 
   # комиссионные за транзакцию
   def self.commission(price)
-    ten_cents = 3.0
+    ten_cents = 2.91
     pcnt = 0.0325
     ((ten_cents + pcnt * price) / ( 1 - pcnt)).round(2)
   end
