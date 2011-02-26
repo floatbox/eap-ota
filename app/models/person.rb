85a238451ef4740fb0056ebc7fb9e0ebaed3d702
codesnik <aronaxis@gmail.com>
2011-02-26 08:22:45 +0300

    разрешил дефисы в имени

--- app/models/person.rb
+++ app/models/person.rb
@@ -16,8 +16,8 @@ class Person < ActiveRecord::BaseWithoutTable
   validates_presence_of :first_name, :last_name, :sex, :nationality_id, :birthday, :passport
   validates_presence_of :document_expiration_date, :unless => :document_noexpiration
   # FIXME WRONG! фамилии через дефис? два имени? сокращения?
-  validates_format_of :first_name, :with => /^[a-zA-Z]*$/, :message => "Некорректное имя"
-  validates_format_of :last_name,  :with => /^[a-zA-Z]*$/, :message => "Некорректная фамилия"
+  validates_format_of :first_name, :with => /^[a-zA-Z-]*$/, :message => "Некорректное имя"
+  validates_format_of :last_name,  :with => /^[a-zA-Z-]*$/, :message => "Некорректная фамилия"
   validate :check_age
   attr_accessor :flight_date, :infant_or_child
 
