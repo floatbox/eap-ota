566939bcf96534a59152d80fb6c146475c0b7d55
abragin <abragin@gmail.com>
2011-03-14 20:12:32 +0300

    geo_ip теперь отваливается по таймауту

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,17 +14,20 @@ class HomeController < ApplicationController
   def nearest_city
     return Location.default unless Conf.geolocation.enabled
     @nearest_city ||= begin
-      loc = GeoIp.geolocation(request.remote_ip)
-      if loc[:latitude] == "0" && loc[:longitude] == "0" ||
-         loc[:latitude] == "60" && loc[:longitude] == "100"
-        return Location.default
+      Timeout.timeout(1) do
+        loc = GeoIp.geolocation(request.remote_ip)
+        if loc[:latitude] == "0" && loc[:longitude] == "0" ||
+          loc[:latitude] == "60" && loc[:longitude] == "100"
+          return Location.default
+        end
+        City.nearest_to loc[:latitude], loc[:longitude]
       end
-      City.nearest_to loc[:latitude], loc[:longitude]
     # для оффлайна
-    rescue Errno::EHOSTUNREACH, SocketError, JSON::ParserError
+    rescue Errno::EHOSTUNREACH, SocketError, JSON::ParserError, Timeout::Error
       Location.default
     end
   end
   helper_method :nearest_city
 
 end
+
