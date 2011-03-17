fdc256a4677bcbac8a41ed66a10ab65a9606ee41
Maxim Zakharov <mszakharov@gmail.com>
2011-03-17 20:07:55 +0300

    Выбор сегмента в диаграмме

--- public/javascripts/results/diagram.js
+++ public/javascripts/results/diagram.js
@@ -1,142 +1,70 @@
 results.diagram = {
 init: function() {
     this.el = $('#offers-diagram');
+    this.header = this.el.find('.odheader');
     this.content = this.el.find('.odcontent');
-    this.width = 730;
+    this.width = 710;
     this.offset = 115;
     this.grid = [15, 30, 60, 90, 120, 180, 240, 360];
     var that = this;
-    this.el.delegate('.segment', 'click', function() {
+    this.content.delegate('.segment', 'click', function() {
         var el = $(this);
         that.selectSegment(el.closest('.odpage').data('segment'), el.attr('data-flights'));
     });
+    this.header.delegate('.odhsegment:not(.selected)', 'click', function() {
+        var el = $(this);
+        that.selectSegment(el.closest('td').data('segment'), undefined, false);
+    });
 },
-selectSegment: function(s, flights) {
-    this.selected[s] = flights;
-    var index = this.segments[s].contents[flights];
-    var properFlights = [];
-    var properVariants = [];
+update: function() {
+    this.content.html('');
+    this.minprice = 0;
+    this.options = $.parseJSON($('#offers-options').attr('data-segments'));
     this.unselected = [];
+    var template = '<div class="odhsegment"><h5>{from} — {to}</h5><h6>{date}</h6></div>';
+    var row = this.header.find('.odhsegments tr').find('td').remove().end();
     for (var s = 0; s < results.samount; s++) {
-        properFlights[s] = {};
-        if (this.selected[s] === undefined) {
-            this.unselected.push(s);
-        }
+        $('<div class="odpage"/>').data('segment', s).hide().appendTo(this.content);
+        $('<td class="odhs' + (s + 1) + '"></td>').data('segment', s).html(template.supplant(this.options[s])).appendTo(row);
+        this.unselected.push[s];
     }
-    for (var i = 0, im = index.length; i < im; i++) {
-        var bars = results.variants[index[i]].bars;
-        var improper = false;
+    this.selected = [];
+    this.selectVariants();
+    this.showSegment(0);
+},
+selectVariants: function() {
+    var variants = results.variants;
+    if (variants[0].bars === undefined) {
+        this.parseBars();
+    }    
+    var selected = this.selected;
+    var proper = [];
+    for (var i = 0, im = variants.length; i < im; i++) {
+        var variant = variants[i];
+        if (variant.improper) continue;
+        var bars = variant.bars, improper = false;
         for (var b = bars.length; b--;) {
-            if (this.selected[b] && this.selected[b] !== bars[b].flights) {
+            if (selected[b] && selected[b] !== bars[b].flights) {
                 improper = true;
                 break;
             }
         }
         if (improper === false) {
-            properVariants.push(index[i]);
-            for (var b = bars.length; b--;) {
-                properFlights[b][bars[b].flights] = true;
-            }
+            proper.push(variant);
         }
     }
-    var lists = this.content.find('.odpage').hide();
-    if (properVariants.length === 1) {
-        var variant = results.variants[properVariants[0]].el.clone();
-        variant.removeClass('g-none').find('.variants').hide();
-        this.el.find('.offer').html('').append(variant).removeClass('latent');
-    } else {
-        var s = this.unselected[0];
-        var segment = this.getSegment(s, properFlights[s]);
-        this.minprice = Math.max(this.minprice, results.variants[properVariants[0]].offer.summary.price);
-        this.updateList(lists.eq(s).html(''), segment);
-        lists.eq(s).show();
-    }
+    this.proper = proper;
 },
-getSegment: function(s, flights) {
-    var that = this;
-    var variants = results.variants;
-    var segment = {
-        items: [],
-        contents: {},
-        prices: {},
-        dpt: 1440,
-        arv: 0,
-    };
-    if (variants[0].bars === undefined) {
-        this.parseBars();
-    }
-    var all = flights === undefined;
-    var shifts = 0;
-    for (var i = 0, im = variants.length; i < im; i++) {
-        var variant = variants[i];
-        var bar = variant.bars[s];
-        var bf = bar.flights;
-        if (!variant.improper && (all || flights[bf] !== undefined)) {
-            if (segment.contents[bf] === undefined) {
-                segment.items.push(bar.el);
-                segment.dpt = Math.min(segment.dpt, bar.dpt);
-                segment.arv = Math.max(segment.arv, bar.arv);
-                segment.contents[bf] = [i];
-                segment.prices[bf] = [variant.offer.summary.price];
-            } else {
-                segment.contents[bf].push(i);
-                segment.prices[bf].push(variant.offer.summary.price);
-            }
-            if (bar.shift !== segment.shift) {
-                segment.shift = bar.shift;
-                shifts++;
-            }
-        }
-    }
-    if (shifts > 1) {
-        segment.shift = 0;
-    }
-    var locations = $.parseJSON($('#offers-options').attr('data-locations'));
-    segment.dptcity = locations[s].from;
-    segment.arvcity = locations[s].to;
-    return segment;
-},
-parseBars: function() {
-    var variants = results.variants;
-    for (var i = variants.length; i--;) {
-        var v = variants[i];
-        v.bars = [];
-        v.el.find('.bars .segment').each(function(s) {
-            var item = $(this);
-            v.bars[s] = {
-                el: item,
-                flights: item.attr('data-flights'),
-                shift: parseInt(item.attr('data-shift'), 10),
-                dpt: parseInt(item.attr('data-dpt'), 10),
-                arv: parseInt(item.attr('data-arv'), 10)
-            };
-        });
-    }
-},
-update: function() {
-    this.content.hide().html('');
-    this.segments = [];
-    this.minprice = 0;
-    this.unselected = [];
-    var samount = results.samount;
-    for (var s = 0; s < results.samount; s++) {
-        var segment = this.getSegment(s);    
-        var list = $('<div class="odpage"/>').data('segment', s);
-        this.updateList(list, segment);
-        this.content.append(list.toggle(s === 0));
-        this.segments[s] = segment;
-    }
-    this.content.show();
-    this.selected = [];
-},
-updateList: function(list, segment) {
-    var that = this;
-    var items = segment.items;
-    var length = segment.arv - segment.dpt;
+showSegment: function(s) {
+    var segment = this.getSegment(s);
+    var list = this.content.find('.odpage').hide().eq(s).html('');
     var locations = $('<ul class="locations"><li>' + segment.dptcity + '</li></ul>').appendTo(list);
     if (segment.shift) {
         locations.append('<li>' + segment.arvcity + '</li>');
     }
+    var items = segment.items;
+    var length = segment.arv - segment.dpt;
+    var that = this;
     for (var i = 0, im = items.length; i < im; i++) {
         var item = items[i].clone();
         item.find('.flight, .layover').each(function() {
@@ -171,8 +99,86 @@ updateList: function(list, segment) {
         if (t === 1440) {
             gline.addClass('midnight');
         }
-        gline.css('left', Math.round((t - segment.dpt) / length * this.width) + this.offset + 50);
+        gline.css('left', Math.round((t - segment.dpt) / length * this.width) + this.offset + 60);
         list.append(gline);
+    }
+    this.header.find('.selected').removeClass('selected');
+    this.header.find('.odhsegment').eq(s).addClass('selected');
+    list.show();
+},
+selectSegment: function(s, flights, auto) {
+    this.selected[s] = flights;
+    this.undselected = [];
+    for (var i = 0; i < results.samount; i++) {
+        if (this.selected[i] === undefined) {
+            this.unselected.push(i);
+        }
+    }
+    this.selectVariants();
+    this.header.find('.selected').removeClass('selected');    
+    if (this.proper.length === 1 && auto !== false) {
+        var variant = this.proper[0].el.clone();
+        variant.removeClass('g-none').find('.variants').hide();
+        this.content.addClass('latent');
+        this.el.find('.offer').html('').append(variant).removeClass('latent');
+    } else {
+        this.el.find('.offer').addClass('latent');
+        this.showSegment(auto === false ? s : this.unselected[0]);
+        this.content.removeClass('latent');
+    }
+},
+getSegment: function(s) {
+    var that = this;
+    var segment = {
+        items: [],
+        contents: {},
+        prices: {},
+        dpt: 1440,
+        arv: 0,
+    };
+    var variants = this.proper;
+    var shifts = 0;
+    for (var i = 0, im = variants.length; i < im; i++) {
+        var variant = variants[i];
+        var bar = variant.bars[s];
+        var bf = bar.flights;
+        if (segment.contents[bf] === undefined) {
+            segment.items.push(bar.el);
+            segment.dpt = Math.min(segment.dpt, bar.dpt);
+            segment.arv = Math.max(segment.arv, bar.arv);
+            segment.contents[bf] = [i];
+            segment.prices[bf] = [variant.offer.summary.price];
+        } else {
+            segment.contents[bf].push(i);
+            segment.prices[bf].push(variant.offer.summary.price);
+        }
+        if (bar.shift !== segment.shift) {
+            segment.shift = bar.shift;
+            shifts++;
+        }
+    }
+    if (shifts > 1) {
+        segment.shift = 0;
+    }
+    segment.dptcity = this.options[s].from;
+    segment.arvcity = this.options[s].to;
+    return segment;
+},
+parseBars: function() {
+    var variants = results.variants;
+    for (var i = variants.length; i--;) {
+        var v = variants[i];
+        v.bars = [];
+        v.el.find('.bars .segment').each(function(s) {
+            var item = $(this);
+            v.bars[s] = {
+                el: item,
+                flights: item.attr('data-flights'),
+                shift: parseInt(item.attr('data-shift'), 10),
+                dpt: parseInt(item.attr('data-dpt'), 10),
+                arv: parseInt(item.attr('data-arv'), 10)
+            };
+        });
     }
 },
 formatTime: function(t) {
