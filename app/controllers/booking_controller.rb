ca0f611d3c6893e9e9270b9575728fcea4203f8f
codesnik <aronaxis@gmail.com>
2011-03-18 12:44:23 +0300

    убрал ненужный хелпер valid_card

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -69,10 +69,5 @@ class BookingController < ApplicationController
     end
   end
 
-  def valid_card
-    {:number => '4111111111111112', :verification_value => '123', :month => 10, :year => 2010, :first_name => 'doesnt', :last_name => 'matter'}
-  end
-  helper_method :valid_card
-
 end
 
