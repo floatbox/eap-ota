edd950b884421d5ef2ed30acaf864afc37f56c3a
codesnik <aronaxis@gmail.com>
2011-03-19 17:51:30 +0300

    еще немножко порядка в спеках

--- spec/sirena/response_describe_spec.rb
+++ spec/sirena/response_describe_spec.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 require 'spec_helper'
 
-describe Sirena::Response::Order do
+describe Sirena::Response::Describe do
+
+  subject { described_class.new( File.read(response) ) }
+
   describe 'with one adult, unpaid' do
 
-    subject {
-      body = File.read('spec/sirena/xml/describe_fail.xml')
-      Sirena::Response::Order.new(body)
-    }
+    let(:response) { 'spec/sirena/xml/describe_fail.xml' }
 
     it { should_not be_success }
     its(:error) { should == "Неправильный формат элемента 'data'" }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,7 @@ Spork.prefork do
     # examples within a transaction, remove the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
+
   end
 
 end
