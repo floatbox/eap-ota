d5c9cb9c6dc5519485e75b7679785baeebf19d13
abragin <abragin@gmail.com>
2011-03-23 17:56:25 +0300

    правильный pnr_cancel, без command_cryptic

---
+++ lib/amadeus/request/pnr_cancel.rb
@@ -0,0 +1,8 @@
+module Amadeus
+  module Request
+    class PNRCancel  < Amadeus::Request::Base
+
+    end
+  end
+end
+
---
+++ lib/amadeus/response/pnr_cancel.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+module Amadeus
+  module Response
+    class PNRCancel < Amadeus::Response::Base
+
+    end
+  end
+end
+
