e5c647a5f3b57b49565ad868e0360770e1f44e9c
abragin <abragin@gmail.com>
2011-03-23 19:23:23 +0300

    fix

--- config/initializers/passenger.rb
+++ config/initializers/passenger.rb
@@ -1,8 +1,6 @@
 if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    Rails.logger.info "First random number: #{rand}"
     srand
-    Rails.logger.info "Second random number: #{rand}"
   end
 end
 
