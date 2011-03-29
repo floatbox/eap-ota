0ddcc211a2670a5d9e63e24484e423772fa9c72a
abragin <abragin@gmail.com>
2011-03-29 20:50:58 +0400

    pricer_controller#hot_offers small change

--- app/controllers/pricer_controller.rb
+++ app/controllers/pricer_controller.rb
@@ -17,7 +17,7 @@ class PricerController < ApplicationController
   end
 
   def hot_offers
-    render :json => HotOffer.find(:all, :conditions => ["code != ? AND for_stats_only = ?", params[:query_key].to_s, false], :limit => 20, :order => 'created_at DESC', :group => 'destination_id')
+    render :json => HotOffer.find(:all, :conditions => ["code != ? AND for_stats_only = ? AND price_variation < 0", params[:query_key].to_s, false], :limit => 20, :order => 'created_at DESC', :group => 'destination_id')
   end
 
   def calendar
