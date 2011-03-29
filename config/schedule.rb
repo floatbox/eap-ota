8e3f2207146baafa278a22db17f170489f357466
codesnik <aronaxis@gmail.com>
2011-03-29 05:00:51 +0400

    убрал бэкапы из schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,8 @@
 # Learn more: http://github.com/javan/whenever
 set :output, "log/cron.log"
 
+job_type :command_at_current, "cd :path && :task :output"
+
 every :day do
   rake 'completer:regen'
 end
@@ -13,14 +15,6 @@ every 10.minutes do
   runner 'Amadeus::Session.housekeep'
 end
 
-every :day do
-  command '/usr/sbin/logrotate -s /home/rack/logrotate/status /home/rack/logrotate/logrotate.conf'
-end
-
-every :day do
-  command 'cd /home/rack/eviterra/shared/backup && (mysqldump -uroot eviterra | gzip > eviterra-`date +\%F`.sql.gz ) && ln -sf eviterra-`date +\%F`.sql.gz latest.sql.gz'
-end
-
 every 1.day, :at => '18:00' do
-  command 'script/cbrusd && touch tmp/restart.txt'
+  command_at_current 'script/cbrusd && touch tmp/restart.txt'
 end
