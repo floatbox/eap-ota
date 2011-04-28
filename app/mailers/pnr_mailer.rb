86fa6df3333cc649b074b33e8bcd0b7eee443fa0
Maxim Zakharov <mszakharov@gmail.com>
2011-04-28 18:12:51 +0400

    Тексты при оплате наличными

--- app/mailers/pnr_mailer.rb
+++ app/mailers/pnr_mailer.rb
@@ -1,12 +1,13 @@
 # encoding: utf-8
 class PnrMailer < ActionMailer::Base
   helper :pricer
+  helper :booking
 
   default :from => "Eviterra.com <ticket@eviterra.com>", :bcc => Conf.mail.ticket_cc
 
   def notification(email, number)
     @pnr = Pnr.get_by_number(number)
-    mail :to => email, :subject => "Ваш электронный билет"
+    mail :to => email, :subject => @pnr.order.payment_type == 'card' ? "Ваш электронный билет" : "Ваше бронирование"
   end
 
   def sirena_receipt(email, number)
