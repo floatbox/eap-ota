4da82ae0b72068960db58577493d44fdf5e5d003
abragin <abragin@gmail.com>
2011-04-29 16:17:09 +0400

    fixes

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class Admin::OrdersController < Admin::ResourcesController
-  before_filter :find_order, :except => [:index, :list, :new, :show, :csv]
+  before_filter :find_order, :only => [:show_pnr, :unblock, :charge, :money_received, :no_money_received, :ticket, :cancel]
 
   def show_pnr
     redirect_to show_order_path(:id => @order.pnr_number)
