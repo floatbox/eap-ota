2b14dd3accc9d20045c731e36e3ebb66a0cdffb6
abragin <abragin@gmail.com>
2011-05-04 16:22:06 +0400

    подкорректированные отчеты по билетам

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,17 @@ class Ticket < ActiveRecord::Base
   belongs_to :order
   delegate :source, :commission_carrier, :description, :to => 'order'
 
+  def ticket_date
+    created_at.strftime('%d.%m.%Y')
+  end
+
+  def price_total
+    price_fare + price_tax
+  end
+
+  def price_transfer
+    price_fare + price_tax + price_consolidator_markup - price_share
+  end
+
 end
 
