0b8d5ae3308aad6ee2de9416d64c8c58602b16a0
codesnik <aronaxis@gmail.com>
2011-05-13 23:39:41 +0400

    сделал describe чуточку для людей

--- lib/sirena/request/describe.rb
+++ lib/sirena/request/describe.rb
@@ -3,6 +3,16 @@ module Sirena
   module Request
     class Describe < Sirena::Request::Base
       attr_accessor :data, :code, :show_all, :company, :show_real_codes
+
+      TYPES = %W[ aircompany airport city country passenger document tax vehicle
+        pcards pcard_types region meal fop ]
+
+      def initialize(data=nil, args={})
+        raise ArgumentError, "first arg should be one of [#{TYPES.join(' ')}]"  unless TYPES.include?(data.to_s)
+        self.data = data
+        super args
+      end
+
     end
   end
 end
