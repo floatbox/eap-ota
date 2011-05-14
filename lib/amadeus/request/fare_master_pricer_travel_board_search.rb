4c983ae9138da72f5f7e1e0a5d04fcad8094d657
codesnik <aronaxis@gmail.com>
2011-05-14 22:42:26 +0400

    упростили поиск для апи

--- lib/amadeus/request/fare_master_pricer_travel_board_search.rb
+++ lib/amadeus/request/fare_master_pricer_travel_board_search.rb
@@ -3,13 +3,14 @@ module Amadeus
     class FareMasterPricerTravelBoardSearch < Amadeus::Request::Base
       attr_accessor :people_count, :nonstop, :segments, :cabin
 
-      def initialize(opts)
+      def initialize(opts, lite=false)
         if opts.is_a? Hash
           super
         else
           @people_count = opts.real_people_count
           @cabin = opts.cabin
           @segments = opts.form_segments
+          @how_many = Conf.amadeus.recommendations_lite if lite
         end
       end
 
