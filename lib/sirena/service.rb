84ab84933d46cf6a8c1672c37a2a140c59aca68d
codesnik <aronaxis@gmail.com>
2011-05-20 20:29:34 +0400

    попытка сделать асинхронную сирену

--- lib/sirena/service.rb
+++ lib/sirena/service.rb
@@ -26,25 +26,48 @@ module Sirena
         response = Sirena::Response.for(name).new(response_body)
       end
 
-      def do_http(request, args={})
-        req = Typhoeus::Request.new "http://#{HOST}:#{PORT}#{PATH}",
-          :method => :post,
-          :body => request,
-          :timeout => 160 * 1000, # in ms
-          :headers => {
-            'X-Encrypt' => (args[:encrypt] ? 'true' : 'false'),
-            'X-Timeout' => ((args[:timeout] || 150) + 5).to_s
-          }
+      def async_action(name, *params, &block)
+        request = Sirena::Request.for(name).new(*params)
+        request_body = request.render
+        log_request(name, request_body)
+        req = make_request(request_body, :encrypt => request.encrypt?)
+        req.on_complete do |response|
+          log_response(name, response.body)
+          Sirena::Response.for(name).new(response.body)
+        end
+        req.after_complete(&block)
+        req
+      end
+
+      # blocking call
+      def do_http(*args)
+        req = make_request(*args)
         Typhoeus::Hydra.hydra.queue req
         Typhoeus::Hydra.hydra.run
-
         req.response.body
+      end
+
+      def make_request(body, opts={})
+        req = Typhoeus::Request.new "http://#{HOST}:#{PORT}#{PATH}",
+          :method => :post,
+          :body => body,
+          :timeout => 160 * 1000, # in ms
+          :headers => {
+            'X-Encrypt' => (opts[:encrypt] ? 'true' : 'false'),
+            'X-Timeout' => ((opts[:timeout] || 150) + 5).to_s
+          }
       end
 
       %W(pricing describe booking booking_cancel payment_ext_auth
          order PNRHistory get_itin_receipts fareremark add_remark pnr_status).each do |method_name|
         define_method method_name.underscore do |*params|
           action(method_name, *params)
+        end
+      end
+
+      %W(pricing).each do |method_name|
+        define_method "async_" + method_name.underscore do |*params|
+          async_action(method_name, *params)
         end
       end
 
