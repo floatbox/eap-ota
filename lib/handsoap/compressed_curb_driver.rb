0c82f56be8bdc69215ad13866b029481af3bac4c
codesnik <aronaxis@gmail.com>
2011-05-31 03:21:01 +0400

    newrelic таймеры

--- lib/handsoap/compressed_curb_driver.rb
+++ lib/handsoap/compressed_curb_driver.rb
@@ -8,6 +8,10 @@ module Handsoap
       curl.encoding = ''
       curl
     end
+
+    include NewRelic::Agent::MethodTracer
+    add_method_tracer :send_http_request, 'Custom/Handsoap/http'
+
   end
 
 end
