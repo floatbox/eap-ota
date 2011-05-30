fc137252b155a6627d7a81a61f01b4328e5246cc
codesnik <aronaxis@gmail.com>
2011-05-30 21:43:02 +0400

    утыкал pricer бенчмарками

--- lib/mux.rb
+++ lib/mux.rb
@@ -7,9 +7,11 @@ class Mux
 
     def pricer(form, admin_user=false, lite=false)
       # FIXME делает сортировку дважды
-      (
-        sirena_pricer(form, admin_user, lite) + amadeus_pricer(form, admin_user, lite)
-      ).sort_by(&:price_total)
+      benchmark 'Pricer, total' do
+        (
+          sirena_pricer(form, admin_user, lite) + amadeus_pricer(form, admin_user, lite)
+        ).sort_by(&:price_total)
+      end
     end
 
     def calendar(form, admin_user=false)
@@ -29,46 +31,57 @@ class Mux
     # TODO exception handling
     def amadeus_pricer(form, admin_user=false, lite=false)
       return [] unless Conf.amadeus.enabled
-      request_ws = Amadeus::Request::FareMasterPricerTravelBoardSearch.new(form, lite)
-      request_ns = Amadeus::Request::FareMasterPricerTravelBoardSearch.new(form, lite)
-      request_ns.nonstop = true
+      benchmark 'Pricer amadeus, total' do
+        request_ws = Amadeus::Request::FareMasterPricerTravelBoardSearch.new(form, lite)
+        request_ns = Amadeus::Request::FareMasterPricerTravelBoardSearch.new(form, lite)
+        request_ns.nonstop = true
 
-      # TODO можно когда-нибудь вернуться. сейчас эта штука _иногда_ одновременно пытается загрузить класс
-      # Country, в разных тредах
-      # a_ws = Amadeus.booking
-      # a_ns = Amadeus.booking
+        # TODO можно когда-нибудь вернуться. сейчас эта штука _иногда_ одновременно пытается загрузить класс
+        # Country, в разных тредах
+        # a_ws = Amadeus.booking
+        # a_ns = Amadeus.booking
 
-      # t_ws = Thread.new { a_ws.fare_master_pricer_travel_board_search(request_ws) }
-      # t_ns = Thread.new { a_ns.fare_master_pricer_travel_board_search(request_ns) }
-      # # игнорируем ошибки, если это конечно не SOAP Error
-      # recommendations_ws = t_ws.value.recommendations
-      # recommendations_ns = t_ns.value.recommendations
-      # a_ws.session.release
-      # a_ns.session.release
+        # t_ws = Thread.new { a_ws.fare_master_pricer_travel_board_search(request_ws) }
+        # t_ns = Thread.new { a_ns.fare_master_pricer_travel_board_search(request_ns) }
+        # # игнорируем ошибки, если это конечно не SOAP Error
+        # recommendations_ws = t_ws.value.recommendations
+        # recommendations_ns = t_ns.value.recommendations
+        # a_ws.session.release
+        # a_ns.session.release
 
-      # non threaded variant
-      recommendations_ws = Amadeus::Service.fare_master_pricer_travel_board_search(request_ws).recommendations
-      #не ловим ошибку, так как может просто не быть беспосадочных вариантов, а это exception
-      recommendations_ns = if Conf.amadeus.nonstop_search && !lite
-                           Amadeus::Service.fare_master_pricer_travel_board_search(request_ns).recommendations
-                           else [] end
+        # non threaded variant
+        recommendations_ws = benchmark 'Pricer amadeus, with stops' do
+          Amadeus::Service.fare_master_pricer_travel_board_search(request_ws).recommendations
+        end
+        #не ловим ошибку, так как может просто не быть беспосадочных вариантов, а это exception
+        recommendations_ns = if Conf.amadeus.nonstop_search && !lite
+            benchmark 'Pricer amadeus, without stops' do
+              Amadeus::Service.fare_master_pricer_travel_board_search(request_ns).recommendations
+            end
+          else
+            []
+          end
 
-      # merge
-      recommendations = Recommendation.merge(recommendations_ws, recommendations_ns)
+        # merge
+        recommendations = []
+        benchmark 'Pricer amadeus, merging and cleanup' do
+          recommendations = Recommendation.merge(recommendations_ws, recommendations_ns)
 
-      # cleanup
-      # TODO рапортовать, если хотя бы одно предложение выброшено
-      # мы вроде что-то делали, чтобы амадеус не возвращал всякие поезда
-      recommendations.delete_if(&:without_full_information?)
-      recommendations.delete_if(&:ground?)
-      recommendations = recommendations.select(&:sellable?) unless admin_user
-      unless lite
-        # sort
-        recommendations = recommendations.sort_by(&:price_total)
-        # regroup
-        recommendations = Recommendation.corrected(recommendations)
+          # cleanup
+          # TODO рапортовать, если хотя бы одно предложение выброшено
+          # мы вроде что-то делали, чтобы амадеус не возвращал всякие поезда
+          recommendations.delete_if(&:without_full_information?)
+          recommendations.delete_if(&:ground?)
+          recommendations = recommendations.select(&:sellable?) unless admin_user
+          unless lite
+            # sort
+            recommendations = recommendations.sort_by(&:price_total)
+            # regroup
+            recommendations = Recommendation.corrected(recommendations)
+          end
+        end
+        recommendations
       end
-      recommendations
     rescue
       notify
       []
@@ -78,8 +91,11 @@ class Mux
       return [] unless Conf.sirena.enabled
       return [] if lite
       return [] unless sirena_searchable?(form)
-      recommendations = Sirena::Service.pricing(form, nil, lite).recommendations || []
-      recommendations.delete_if(&:without_full_information?)
+      recommendations = []
+      benchmark 'Pricer sirena' do
+        recommendations = Sirena::Service.pricing(form, nil, lite).recommendations || []
+        recommendations.delete_if(&:without_full_information?)
+      end
       recommendations
     rescue
       notify
@@ -91,12 +107,13 @@ class Mux
       return false if form.form_segments.size > 2
       return false if form.form_segments.any? {|fs| fs.from_as_object == fs.to_as_object}
       locations = (form.form_segments.every.from_as_object + form.form_segments.every.to_as_object).uniq
-	  !Conf.sirena.restrict ||
-	  # form.adults == 1 &&
-	  form.children == 0 && form.infants == 0 &&
-	  form.form_segments.none?(&:multicity?) &&
-	  locations.all?{|l| l.iata_ru != l.iata && l.iata_ru.present?} &&
-	  locations.any?{|l| SNG_COUNTRY_CODES.include? l.country.alpha2}
+
+      !Conf.sirena.restrict ||
+        # form.adults == 1 &&
+        form.children == 0 && form.infants == 0 &&
+        form.form_segments.none?(&:multicity?) &&
+        locations.all?{|l| l.iata_ru != l.iata && l.iata_ru.present?} &&
+        locations.any?{|l| SNG_COUNTRY_CODES.include? l.country.alpha2}
     end
 
     # report error and continue
@@ -112,6 +129,12 @@ class Mux
         Rails.logger.error("#{message}\n  ...reported and continued\n")
       end
       HoptoadNotifier.notify(exception) rescue Rails.logger.error("  can't notify hoptoad #{$!.class}: #{$!.message}")
+    end
+
+    include ActiveSupport::Benchmarkable
+
+    def logger
+      Rails.logger
     end
 
   end
