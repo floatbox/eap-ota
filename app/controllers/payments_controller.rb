cc83454329c3565766ef7cadfc41cd7c3170c96f
codesnik <aronaxis@gmail.com>
2011-06-17 17:13:40 +0400

    обработка обработанного отложеннего платежа

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,12 +2,27 @@
 class PaymentsController < ApplicationController
 
   def edit
-    @order = Order.first(:conditions => ["code = ? AND payment_status = 'not blocked' AND offline_booking = ?", params[:code], true])
-    @card = CreditCard.new
+    unless params[:code] and @order = Order.where(:offline_booking => true, :code => params[:code]).first
+      render :status => :not_found
+      return
+    end
+
+    if @order.payment_status == 'not blocked'
+      @card = CreditCard.new
+    end
   end
 
   def update
-    @order = Order.first(:conditions => ["code = ? AND payment_status = 'not blocked' AND offline_booking = ?", params[:code], true])
+    unless params[:code] and @order = Order.where(:offline_booking => true, :code => params[:code]).first
+      render :status => :not_found
+      return
+    end
+
+    unless @order.payment_status == 'not blocked'
+      render :partial => 'success'
+      return
+    end
+
     card = CreditCard.new(params[:card])
     if card.valid?
       payture_response = @order.block_money(card)
@@ -16,7 +31,7 @@ class PaymentsController < ApplicationController
         render :partial => 'success'
       elsif payture_response.threeds?
         logger.info "Pay: payment system requested 3D-Secure authorization"
-        render :partial => 'booking/threeds', :locals => {:order_id => @order.order_id, :payture_response => payture_response}
+        render :partial => 'booking/threeds', :locals => {:payture_response => payture_response}
       else
         logger.info "Pay: payment failed"
         render :partial => 'fail'
