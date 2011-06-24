e20d64821ddf2f0e01b45013fd96c431c8a706de
Andrey Arefiev <arefiev@gmail.com>
2011-06-24 20:57:24 +0400

    новый URL api для yandex

--- app/helpers/yandex_helper.rb
+++ app/helpers/yandex_helper.rb
@@ -18,4 +18,8 @@ module YandexHelper
   def yandex_url(search)
     "https://eviterra.com/##{@search.query_key}"
   end
+  
+  def yandex_newurl(search, recommendation, variant)
+    "https://eviterra.com/api/booking?query_key=#{@search.query_key}&recommendation=#{recommendation.serialize(variant)}"
+  end
 end
