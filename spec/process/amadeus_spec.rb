2d11eb8dabec37de7841a0d0a9ec81d272deff79
codesnik <aronaxis@gmail.com>
2011-06-30 11:53:09 +0400

    fixed amadeus spec

--- spec/process/amadeus_spec.rb
+++ spec/process/amadeus_spec.rb
@@ -4,8 +4,12 @@ require 'spec_helper'
 describe Amadeus::Service do
   use_vcr_cassette 'amadeus_cmd', :record => :new_episodes
 
+  it "should require session" do
+    expect { Amadeus::Service.cmd('help') }.to raise_error
+  end
+
   it "should be working" do
-    Amadeus::Service.cmd('help').should =~ /HELP/
+    Amadeus.booking.cmd('help').should =~ /HELP/
   end
 end
 
