5b9c28eb098d9151b0e53e96c63e282ef6fb6c5d
abragin <abragin@gmail.com>
2011-07-14 20:20:36 +0400

    временно выключил поиск в радиусе для сложных маршрутов

--- lib/amadeus/request/fare_master_pricer_travel_board_search.rb
+++ lib/amadeus/request/fare_master_pricer_travel_board_search.rb
@@ -1,7 +1,7 @@
 module Amadeus
   module Request
     class FareMasterPricerTravelBoardSearch < Amadeus::Request::Base
-      attr_accessor :people_count, :nonstop, :segments, :cabin, :lite, :suggested_limit
+      attr_accessor :people_count, :nonstop, :segments, :cabin, :lite, :suggested_limit, :complex_route
 
       def initialize(*args)
         unless args.first.is_a?(Hash)
@@ -9,6 +9,7 @@ module Amadeus
           @people_count = form.real_people_count
           @cabin = form.cabin
           @segments = form.form_segments
+          @complex_route = form.complex_route?
         end
         super *args
       end
