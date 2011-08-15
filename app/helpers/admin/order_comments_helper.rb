c3c634793f87d854ac13ce8cd58c3bcbb4fbcb8b
abragin <abragin@gmail.com>
2011-08-15 19:16:42 +0400

    fix integer_filter в тайпусе

--- app/helpers/admin/order_comments_helper.rb
+++ app/helpers/admin/order_comments_helper.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
 module Admin::OrderCommentsHelper
   include ActionView::Helpers::TextHelper
-
+  include Admin::Resources::DataTypes::StringHelper
+  alias_method :integer_filter, :string_filter
 # переопредилил метод truncate для нормального показа комментариев к заказу в админке 
   def truncate(text, options = {})
 #    super(text, :length => 100)
     simple_format(text)
   end
+
 end
