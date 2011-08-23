c53a11bdbdc8f6b6f4b9fd53aff4423a2b32e0b4
codesnik <aronaxis@gmail.com>
2011-08-23 12:26:10 +0400

    тесты для тикетов. finally

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Ticket do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:ticket) {
+    described_class.new :code => '29A', :number => '1234567890-91'
+  }
+
+  subject {ticket}
+  its(:number_with_code) {should == '29A-1234567890-91'}
 end
