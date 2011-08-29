54a29798ec500839842293ab45a1e215a5e406c3
codesnik <aronaxis@gmail.com>
2011-08-29 21:43:18 +0400

    TicketDisplayTST fares_count, total_fare, total_tax

--- spec/amadeus/ticket_display_tst_spec.rb
+++ spec/amadeus/ticket_display_tst_spec.rb
@@ -14,8 +14,11 @@ describe Amadeus::Response::TicketDisplayTST do
     specify { subject.prices_with_refs.should have(1).keys }
     specify { subject.prices_with_refs.values[0].should have(4).keys }
     specify { subject.prices_with_refs.values[0].should == {:price_fare=>5985, :price_tax=>656, :price_fare_infant => 600, :price_tax_infant => 171}}
+    its(:total_fare) { should == 6585 }
+    its(:total_tax) { should == 827 }
     its(:validating_carrier_code) { should == 'FV' }
     its(:last_tkt_date) { should == Date.new(2011, 4, 14) }
+    its(:fares_count) { should == 2 }
   end
 end
 
