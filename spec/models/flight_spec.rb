ce64554c589f4935bc622320ec16423bb4d227dc
codesnik <aronaxis@gmail.com>
2011-08-29 21:18:32 +0400

    зачинил Сашины тесты

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,19 +6,32 @@ describe Flight do
     expect { Flight.from_flight_code('ЮТ369ВНКПЛК060711') }.to_not raise_error
   end
 
-  describe "#from_amadeus_code" do
-    it "should call air_flight_info with correct params" do
-      code = '3  BT 419 Z 01AUG 4 DMERIX HK1  1505 1600  01AUG  E  BT/22BFU3'
-      Amadeus::Service.should_receive(:air_flight_info).with(:date => '010812', :number => '419', :carrier => 'BT', :departure_iata => 'DME', :arrival_iata => 'RIX')
-      Flight.from_amadeus_code(code)
-    end
-  end
+  context "Amadeus specific code" do
+    # вероятно, сломается на чем-то более серьезном чем тесты ниже
+    let (:stubbed_air_flight_info) {
+      Object.new.tap {|air_flight_info| air_flight_info.stub(:flight) }
+    }
 
-  describe '#from_short_code' do
-    it "should call air_flight_info with correct params" do
-      code = 'BT419 010812'
-      Amadeus::Service.should_receive(:air_flight_info).with(:date => '010812', :number => '419', :carrier => 'BT')
-      Flight.from_short_code(code)
+    describe "#from_amadeus_code" do
+      it "should call air_flight_info with correct params" do
+        code = '3  BT 419 Z 01AUG 4 DMERIX HK1  1505 1600  01AUG  E  BT/22BFU3'
+        Amadeus::Service.should_receive(:air_flight_info)\
+          .with(:date => '010812', :number => '419', :carrier => 'BT', :departure_iata => 'DME', :arrival_iata => 'RIX')\
+          .and_return(stubbed_air_flight_info)
+
+        Flight.from_amadeus_code(code)
+      end
+    end
+
+    describe '#from_short_code' do
+      it "should call air_flight_info with correct params" do
+        code = 'BT419 010812'
+        Amadeus::Service.should_receive(:air_flight_info)\
+          .with(:date => '010812', :number => '419', :carrier => 'BT')\
+          .and_return(stubbed_air_flight_info)
+
+        Flight.from_short_code(code)
+      end
     end
   end
 
