138556f80107a2f6f07f86cf5684a7f2026b6ea9
codesnik <aronaxis@gmail.com>
2011-09-03 14:35:44 +0400

    подсказки из GDS при редактировании городов, аэропортов, авиакомпаний и самолетов

--- app/controllers/admin/airports_controller.rb
+++ app/controllers/admin/airports_controller.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 class Admin::AirportsController < Admin::ResourcesController
+  include Admin::DecodingController
+  self.sirena_dictionary = :airport
+  self.amadeus_dictionary = :dac
+
   def update_completer
     Completer.regen
     redirect_to :action => :list
