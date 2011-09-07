8589178a8b7f37893b15823e246d0a1c3bba2f16
codesnik <aronaxis@gmail.com>
2011-09-07 15:20:56 +0400

    это конечно нехорошо, но так всем будет удобнее

--- db/migrate/20110907055602_add_source_to_tickets.rb
+++ db/migrate/20110907055602_add_source_to_tickets.rb
@@ -2,5 +2,13 @@ class AddSourceToTickets < ActiveRecord::Migration
   def up
     add_column :tickets, :pnr_number, :string, :first => true
     add_column :tickets, :source, :string, :first => true
+    # хоть и дата миграция, но
+    # в данном случае, относительно безопасно
+    execute "update tickets, orders set tickets.pnr_number = orders.pnr_number, tickets.source = orders.source where tickets.order_id = orders.id"
+  end
+
+  def down
+    remove_column :tickets, :pnr_number
+    remove_column :tickets, :source
   end
 end
