347a77c9d59dec83ce436ba0cbbe6811cea6573e
abragin <abragin@gmail.com>
2011-09-15 20:14:01 +0400

    fix спеков

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,7 +5,7 @@ describe Order do
   describe '#load_tickets' do
     it 'loads tickets correctly from amadeus' do
       order = Order.new(:source => 'amadeus', :commission_subagent => '1%', :pnr_number => '123456')
-      order.stub_chain(:tickets, :every, :delete)
+      order.stub_chain(:tickets, :where, :every, :update_attribute)
       amadeus = mock('Amadeus')
 
       body = File.read('spec/amadeus/xml/PNR_Retrieve_with_ticket.xml')
