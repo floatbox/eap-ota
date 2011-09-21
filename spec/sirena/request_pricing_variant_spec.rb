f49e184b103546e83ca6fa331594ce720913cac9
Kate <peatrovich@gmail.com>
2011-09-21 18:05:15 +0400

    полечила спеку

--- spec/sirena/request_pricing_variant_spec.rb
+++ spec/sirena/request_pricing_variant_spec.rb
@@ -5,24 +5,25 @@ describe Sirena::Request::PricingVariant do
 
   describe 'проверяем правильность работы конструктора' do
 
-    let (:form) {PricerForm.simple(:from => "MOV", :to => "LED", :date1 => "290911", :date2 => "300911", :infants => 1, :children => 1, :cabin => "Y")
-    let (:order) {Sirena::Service.new.order('08РКЦЦ','MAREEVA')}
-    let (:recommendation) {Order.new.recommendation_for_sirena(order)}
+    let(:form){PricerForm.simple(:from => "MOV", :to => "LED", :date1 => "290911", :date2 => "300911", :infants => 1, :children => 1, :cabin => "Y")}
+    let(:order){Sirena::Response::Order( File.read(response) )}
+    let(:recommendation){Order.new.recommendation_for_sirena(order)}
+    let(:response) { 'spec/sirena/xml/order_for_pricing.xml' }
 
     context "#priceform_given" do
       subject {described_class.new form}
 
-      it{should have(1).passengers}
-      specify { subject.passengers[0].code.should == 'ААА'  }
-      specify { subject.passengers[0].count.should == 1
+      it{should have(3).passengers}
+      specify { subject.passengers[0][:code].should == 'ААА'  }
+      specify { subject.passengers[0][:count].should == 1 }
     end
 
     context "#recommendation_given" do
       subject {described_class.new recommendation}
 
-      it{should have(1).passengers}
-      specify { subject.passengers[0].code.should == 'ААА'  }
-      specify { subject.passengers[0].count.should == 1}
+      it{should have(3).passengers}
+      specify { subject.passengers[0][:code].should == 'ААА'  }
+      specify { subject.passengers[0][:count].should == 1}
     end
   end
 end
