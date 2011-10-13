9414549c6b3d0990d3047a4a218095bf17390d8d
abragin <abragin@gmail.com>
2011-10-13 16:25:59 +0400

    теперь все подписки идут с копией на subscription@

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -3,7 +3,7 @@ class SubscriptionMailer < ActionMailer::Base
   #run queu > bundle exec rake qu:work QUEUES=subscription
 
   add_template_helper(ApplicationHelper)
-  default :from => "Eviterra.com <ticket@eviterra.com>"
+  default :from => "Eviterra.com <ticket@eviterra.com>", :bcc => Conf.mail.subscription_cc
 
   def notice(notice_info)
     @notice = notice_info
