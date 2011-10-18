98fa1ca0509bc4008aad6211c23c12e4f26915fc
Kate <peatrovich@gmail.com>
2011-10-18 18:25:42 +0400

    промежуточный коммит

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,37 +4,27 @@ class PaymentsController < ApplicationController
   before_filter :find_order
 
   def find_order
-    @order = Order.find_by_code!( params[:code].presence || 'not specified' )
+    @order = Order.find_by_code!( params[:code].presence )
   end
 
   def edit
-    unless @order.last_pay_time >= Time.now
-      render "expired_pay_time"
-      return
-    end
-
-    unless @order.offline_booking
-      render 'error', :status => 404
+    if !( @order.last_pay_time >= Time.now || @order.offline_booking )
+      render "expired_pay_time" , :status => 404
       return
     end
 
     if @order.payment_status == 'not blocked'
-      @card = CreditCard.new
+       @card = CreditCard.new
     end
   end
 
   def update
-    unless @order.last_pay_time >= Time.now
-      render "expired_pay_time"
+    if !( @order.last_pay_time >= Time.now || @order.payment_status == 'not blocked' )
+      render "expired_pay_time", :status => 404
       return
     end
 
-    unless @order.offline_booking
-      render 'error', :status => 404
-      return
-    end
-
-    unless @order.payment_status == 'not blocked'
+    unless
       render :partial => 'success'
       return
     end
