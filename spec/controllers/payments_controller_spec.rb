e4401f548d25f745e788c7781d37b6d0169d8362
Kate <peatrovich@gmail.com>
2011-10-20 19:00:15 +0400

    то, что спека не ругалась на второй аргумент, создало иллюзию, что она его проверяет. оказалось, ей просто похуй. убрала, чтобы не вводить в заблуждение, до тех пор, пока не найду метод проверки статуса.

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -9,7 +9,7 @@ describe PaymentsController do
       record = mock_model(Order, :code => '47b4l', :offline_booking => false, :last_pay_time => Date.tomorrow, :payment_status => '')
       Order.stub(:find_by_code!).and_return(record)
       post :update, :code=> '47b4l'
-      response.should render_template('expired_pay_time', '404')
+      response.should render_template('expired_pay_time')
     end
 
 
@@ -18,7 +18,7 @@ describe PaymentsController do
       record = mock_model(Order, :code=> '47b4l', :last_pay_time => Date.yesterday, :offline_booking => true, :payment_status => '')
       Order.stub(:find_by_code!).and_return(record)
       post :update, :code => '47b4l'
-      response.should render_template('expired_pay_time', '404')
+      response.should render_template('expired_pay_time')
     end
 
   end
@@ -30,7 +30,7 @@ describe PaymentsController do
       record = mock_model(Order, :code => '47b4l', :offline_booking => false, :last_pay_time => Date.tomorrow, :payment_status => '')
       Order.stub(:find_by_code!).and_return(record)
       get :edit, :code => '47b4l'
-      response.should render_template('expired_pay_time', '404')
+      response.should render_template('expired_pay_time')
     end
 
     it 'shows error page with 404 status if last_pay_time has expired' do
@@ -38,7 +38,7 @@ describe PaymentsController do
       record = mock_model(Order, :code=> '47b4l', :last_pay_time => Date.yesterday, :offline_booking => true, :payment_status => '')
       Order.stub(:find_by_code!).and_return(record)
       get :edit, :code => '47b4l'
-      response.should render_template('expired_pay_time', '404')
+      response.should render_template('expired_pay_time')
     end
 
   end
