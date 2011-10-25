bd21b494f63abeef370e032a06490b9f0b1580de
codesnik <aronaxis@gmail.com>
2011-10-25 12:08:50 +0400

    важный коммент к rambler_cache.rb

--- app/models/rambler_cache.rb
+++ app/models/rambler_cache.rb
@@ -1,4 +1,9 @@
 class RamblerCache
+  # на продакшне хранится в capped collection
+  # таблицу надо создавать явно:
+  # db.createCollection("rambler_caches", {capped:true, size:200000000})
+  # удалять объекты нельзя. обновлять можно только, если не увеличивается размер документа
+  # TODO сделать флажок об отправке, заранее сохраненный
   include Mongoid::Document
   include Mongoid::Timestamps
   field :data, :type => Array
