846ba0cec7cf969d29ba37f54b9e299c9e40d85e
codesnik <aronaxis@gmail.com>
2011-11-04 06:10:23 +0400

    редирект на show после сохранения в админке

--- app/controllers/admin/eviterra_resource_controller.rb
+++ app/controllers/admin/eviterra_resource_controller.rb
@@ -12,4 +12,23 @@ class Admin::EviterraResourceController < Admin::ResourcesController
     request.env['eviterra.admin_user'] = admin_user.email if admin_user
   end
 
+  # патчим редирект после сохранения на show
+  def redirect_on_success
+    path = params.dup.cleanup
+
+    options = if params[:_save]
+      # переход на show, а не на индекс
+      { :action => 'show', :id => @item.id }
+    elsif params[:_addanother]
+      { :action => 'new', :id => nil }
+    elsif params[:_continue]
+      { :action => 'edit', :id => @item.id }
+    end
+
+    message = params[:action].eql?('create') ? "%{model} successfully created." : "%{model} successfully updated."
+    notice = Typus::I18n.t(message, :model => @resource.model_name.human)
+
+    redirect_to path.merge!(options).compact, :notice => notice
+  end
+
 end
