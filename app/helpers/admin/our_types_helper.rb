0a394d744521ded22a88a025f0b47431c205bb5c
codesnik <aronaxis@gmail.com>
2011-11-10 21:54:35 +0400

    отображение в админке Float как BigDecimal

--- app/helpers/admin/our_types_helper.rb
+++ app/helpers/admin/our_types_helper.rb
@@ -4,11 +4,14 @@ module Admin::OurTypesHelper
   # decimal у нас сейчас почти исключительно для сумм в рублях. потом что-то другое придумаем
   def display_decimal(item, attribute)
     val = item.send(attribute)
-    if val.frac.zero?
-      "%.f р." % val
-    else
-      "%.2f р." % val
-    end
+    # ломается на Float
+    #if val.frac.zero?
+    #  "%.f р." % val
+    #else
+    #  "%.2f р." % val
+    #end
+    # грубо, но должно работать
+    ("%.2f р." % val).sub(/\.00/, '')
   end
 
   def table_decimal_field(attribute, item)
