c0599006cbd396174849a4d9895f2b369b4ba870
Kate <peatrovich@gmail.com>
2011-11-16 17:31:11 +0400

    забыла файл

--- lib/rambler_api.rb
+++ lib/rambler_api.rb
@@ -7,13 +7,6 @@ class RamblerApi
   def initialize params
     date1 = PricerForm.convert_api_date(params[:request][:dir])
     date2 = PricerForm.convert_api_date(params[:request][:ret])
-    cabin = params[:request][:cls]
-    if cabin == 'P'|| cabin == 'B'
-      cabin = 'C'
-    elsif cabin == 'E' || cabin == 'A'
-      cabin = 'Y'
-    end
-
     pricer_form_hash = {
         :from => params[:request][:src],
         :to => params[:request][:dst],
@@ -22,28 +15,27 @@ class RamblerApi
         :adults => params[:request][:adt].to_i,
         :children => params[:request][:cnn].to_i,
         :infants => params[:request][:inf].to_i,
-        :cabin => cabin,
+        :cabin => convert_cabin_char(params[:request][:cls]),
         :partner => 'rambler'
     }
     self.search = PricerForm.simple(pricer_form_hash)
-
-    segments = (params[:response][:dir] + (params[:response][:ret] || [])).collect do |segment|
+    received_segments = params[:response][:dir] + (params[:response][:ret] || [])
+    segments = received_segments.collect do |segment|
       Segment.new(:flights =>
         [Flight.new(
           :operating_carrier_iata => segment[:oa],
           :marketing_carrier_iata => segment[:ma],
           :flight_number => segment[:n],
-          :booking_class => segment[:bcl],
-          :cabin => segment[:cls],
           :departure_iata => params[:request][:src],
           :arrival_iata => params[:request][:dst],
           :departure_date => date1 )])
       end
     variants = Variant.new(:segments => segments)
 
-    booking_classes, cabins = segments.each do |segment|
-      booking_classes = segment.flights.collect(&:booking_class)
-      cabins = segment.flights.collect(&:cabin)
+    booking_classes, cabins =  [], []
+    booking_classes, cabins = received_segments.each do |segment|
+      booking_classes << segment[:bcl]
+      cabins << convert_cabin_char(segment[:cls])
       break booking_classes, cabins
     end
 
@@ -55,5 +47,13 @@ class RamblerApi
       :variants => [variants]
     )
     self.recommendation = recommendation.serialize
+  end
+
+  def convert_cabin_char received_cabin
+    if received_cabin == 'P'|| received_cabin == 'B'
+      'C'
+    elsif received_cabin == 'E' || received_cabin == 'A'
+      'Y'
+    end
   end
 end
