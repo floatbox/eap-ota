7d8d0b6d0009126cbda3f345b04d3b8a70e8acb1
waaa <peatrovich@gmail.com>
2011-12-09 21:07:37 +0400

    все, закончила с хот-офферами whooooraai

--- app/controllers/admin/hot_offers_controller.rb
+++ app/controllers/admin/hot_offers_controller.rb
@@ -1,3 +1,8 @@
 # encoding: utf-8
-class Admin::HotOffersController < Admin::EviterraResourceController
+class Admin::HotOffersController < Admin::BaseController
+  def best_of_the_week
+     @hot_offers = HotOffer.where(:created_at.gte => (Date.today-7.days)).and(:rt => true).and(:price_variation_percent => 15...45).order_by(:price_variation_percent => :desc)
+     @hot_offers.all.group_by{ |h|  h.to_iata && h.price}.values.every.first
+     render :index
+  end
 end
