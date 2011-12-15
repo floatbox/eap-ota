0a47ee65910d57e7d114cd056e1edfc473533729
waaa <peatrovich@gmail.com>
2011-12-15 18:24:31 +0400

    скрипт для выгрузки прайсеров

---
+++ lib/pricer_dump.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require File.expand_path('../../config/environment',  __FILE__)
+require 'fileutils'
+
+class PricerDump
+  def self.create_dump
+    FileUtils.touch 'log/pricer_dump.txt'
+    File.open('log/pricer_dump.txt', 'w') do |f|
+       PricerForm.all.each_with_index do |pf, i|
+         from = pf.from_iata + ' '*(4-pf.from_iata.scan(/./u).length)
+         to = pf.to_iata + ' '*(4-pf.to_iata.scan(/./u).length)
+         f.puts "#{from} #{to} #{pf.segments.first.date} #{pf.rt ? 'rt':'ow'}\n"
+         f.puts ".\n" if i % 1000 == 0
+    end end
+  end
+end
+PricerDump.create_dump
