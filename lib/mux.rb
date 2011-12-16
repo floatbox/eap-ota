1acb0cde0a05b25a782c5db388f2fd82d60c0450
abragin <abragin@gmail.com>
2011-12-16 18:32:05 +0400

    удалил ЮХ из сирены

--- lib/mux.rb
+++ lib/mux.rb
@@ -131,7 +131,7 @@ class Mux
   def sirena_cleanup(recs)
     recs.delete_if(&:without_full_information?)
     # временно из-за проблем с тарифами AB и LX удаляем из рекоммендации
-    recs.delete_if {|r| ['AB', 'LX'].include? r.validating_carrier_iata }
+    recs.delete_if {|r| ['AB', 'LX', 'ЮХ'].include? r.validating_carrier_iata }
     recs.every.clear_variants
     recs.delete_if{|r| r.variants.blank?}
   end
