70263e3f904f92767d3e0051fadbc1dbf72d2ccd
Andrey Arefiev <arefiev@gmail.com>
2012-01-16 18:56:42 +0400

    обновление формы подписки с данными destination

--- app/assets/javascripts/results/results.js
+++ app/assets/javascripts/results/results.js
@@ -872,10 +872,16 @@ send: function() {
         return false;
     }
     this.button.get(0).disabled = true;
+    console.log({from_iata: this.el.attr('data-from-iata'),
+        to_iata: this.el.attr('data-to-iata'),
+        rt: this.el.attr('data-rt')});
     $.ajax({
         url: '/subscribe/',
         data: {
             destination_id: this.el.attr('data-destination'),
+            from_iata: this.el.attr('data-from-iata'),
+            to_iata: this.el.attr('data-to-iata'),
+            rt: this.el.attr('data-rt'),
             email: value
         },
         success: function(s) {
