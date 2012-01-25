006962ffbf9f2e8693c1ef27f2f9f241d477c31f
waaa <peatrovich@gmail.com>
2012-01-25 19:00:52 +0400

    потерялся модуль

---
+++ lib/partner_module.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+module PartnerModule
+
+  def track_partner partner, marker=nil
+    unless cookies[:partner]
+      StatCounters.inc %W[enter.api.first_time enter.api.#{partner}.first_time]
+      logger.info "API::Partner::Enter: #{partner} #{Time.now}"
+      logger.info "API::Marker::Enter: #{marker} #{Time.now}"
+    end
+    cookies[:partner] = { :value => partner, :expires => Time.now + 3600*24*30}
+    cookies[:marker] = { :value => marker, :expires => Time.now + 3600*24*30} if marker
+  end
+
+  def check_partner
+    cookies[:partner]
+  end
+
+  def check_marker
+    cookies[:marker]
+  end
+
+  def log_partner
+    logger.info "API::Partner: #{cookies[:partner]}" if cookies[:partner]
+  end
+
+  def log_marker
+    logger.info "API::Marker: #{cookies[:marker]}" if cookies[:marker]
+  end
+end
