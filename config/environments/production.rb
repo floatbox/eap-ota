a0e4cebaddc12c45c433939ebe3c4de540f7ea1d
codesnik <aronaxis@gmail.com>
2012-01-26 20:00:20 +0400

    включил обратно компиляцию на лету

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Eviterra::Application.configure do
   config.serve_static_assets = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
