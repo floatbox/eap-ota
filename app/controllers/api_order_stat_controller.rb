63a9d1d35a71271a21ce076cda829053da812476
waaa <peatrovich@gmail.com>
2012-01-27 19:42:08 +0400

    passwords в defaults.yml и правка api_order_stat контроллера

--- app/controllers/api_order_stat_controller.rb
+++ app/controllers/api_order_stat_controller.rb
@@ -1,29 +1,28 @@
 # encoding: utf-8
 class ApiOrderStatController < ApplicationController
-  USER_ID, PASSWORD = "yandex", "admin"
   before_filter :authenticate
 
   def index
-    orders = Order.where(:created_at=>Date.parse(params[:start_date])..Date.parse(params[:end_date]), :partner => USER_ID)
-    order_to_send = {}
-    orders_to_send = []
-    orders.each do |order|
-      order_to_send[:marker] = order.marker
-      order_to_send[:price] = order.price_with_payment_commission
-      order_to_send[:income] = order.income
-      order_to_send[:created_at] = order.created_at
-      order_to_send[:route] = order.route
-      order_to_send[:partner] = order.partner #для теста!!! убрать
-      orders_to_send << order_to_send
+    orders = Order.where(:created_at=>Date.parse(params[:date_start])..Date.parse(params[:date_end]), :partner => @id)
+    orders_to_send = orders.map do |order|
+      {
+      :marker => order.marker,
+      :price => order.price_with_payment_commission,
+      :income => order.income,
+      :created_at => order.created_at,
+      :route => order.route
+      }
     end
-    orders_to_send = orders_to_send.to_json
-    render :json => orders_to_send
+    render :json => orders_to_send.to_json
   end
 
   private
     def authenticate
       authenticate_or_request_with_http_basic do |id, password|
-        id == USER_ID && password == PASSWORD
+        Conf.api.passwords.include? id && password == Conf.api.passwords[id]
+        @id = id
     end
    end
 end
+
+
