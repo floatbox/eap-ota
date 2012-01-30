b989b73bed76fcd1b6bd4bbbe46b3a4188092e88
codesnik <aronaxis@gmail.com>
2012-01-30 23:59:49 +0400

    rails 3.2.1, + обновления config/*

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
-gem 'rails', '3.1.3'
+gem 'rails', '3.2.1'
 
 gem 'jquery-rails'
 group :assets do
--- lib/geo_taggable.rb
+++ lib/geo_taggable.rb
@@ -1,22 +1,12 @@
 # encoding: utf-8
 module GeoTaggable
-  def self.included(base)
-    base.send :include, InstanceMethods
-    base.extend ClassMethods
+  def geo_tag_list
+    geo_tags.map(&:name).join(', ')
   end
 
-  module InstanceMethods
-    def geo_tag_list
-      geo_tags.map(&:name).join(', ')
-    end
-
-    def geo_tag_list=(list)
-      tag_names = list.split(/,\s*/)
-      geo_tags.map(&:name).join(', ')
-      self.geo_tags = tag_names.map {|tag_name| GeoTag.find_or_create_by_name(tag_name)}
-    end
-  end
-
-  module ClassMethods
+  def geo_tag_list=(list)
+    tag_names = list.split(/,\s*/)
+    geo_tags.map(&:name).join(', ')
+    self.geo_tags = tag_names.map {|tag_name| GeoTag.find_or_create_by_name(tag_name)}
   end
 end
