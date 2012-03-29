e9babc0175afad9a244332571e605b8bbad03b2c
Andrey Arefiev <arefiev@gmail.com>
2012-03-29 20:48:32 +0400

    добавил итоговую сумму в счетчики

--- app/controllers/admin/counters_controller.rb
+++ app/controllers/admin/counters_controller.rb
@@ -6,13 +6,19 @@ class Admin::CountersController < Admin::BaseController
   end
 
   def destinations
-    count = 1000
+    count = 1500
     @tabs = (0..6).to_a.reverse.map {|t| t.days.ago.strftime(StatCounters::DATE_FORMAT) }
     @data = []
     @tabs.each do |date|
       day_data = []
-      StatCounters.search_on_date(date, count).each {|i| day_data << i}
-      @data << { :title => date, :day_data => day_data }
+      total_s = 0
+      total_e = 0
+      StatCounters.search_on_date(date, count).each do |i|
+        day_data << i
+        total_s = total_s + i['search']['api']['total'] if !i['search'].blank?
+        total_e = total_e + i['enter']['api']['total'] if !i['enter'].blank?
+      end
+      @data << { :title => date, :day_data => day_data, :total_s => total_s, :total_e => total_e }
     end
   end
 
