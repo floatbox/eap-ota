1f194ad1427f8d573aedd962d6cb68d4424b6147
Andrey Arefiev <arefiev@gmail.com>
2012-03-30 00:50:39 +0400

    добавил в счетчики проверки на пустоту

--- app/controllers/admin/counters_controller.rb
+++ app/controllers/admin/counters_controller.rb
@@ -15,8 +15,8 @@ class Admin::CountersController < Admin::BaseController
       total_e = 0
       StatCounters.search_on_date(date, count).each do |i|
         day_data << i
-        total_s = total_s + i['search']['api']['total'] if !i['search'].blank?
-        total_e = total_e + i['enter']['api']['total'] if !i['enter'].blank?
+        total_s = total_s + i['search']['api']['total'] if i['search']['api']
+        total_e = total_e + i['enter']['api']['total'] if i['enter']
       end
       @data << { :title => date, :day_data => day_data, :total_s => total_s, :total_e => total_e }
     end
