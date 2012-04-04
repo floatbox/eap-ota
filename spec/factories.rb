79ad456a47656e685040d7196f20e5961fda1b54
codesnik <aronaxis@gmail.com>
2012-04-04 16:35:11 +0400

    новые Factory, и метод "factories!" для rails console

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,51 @@
 # documentation: https://github.com/thoughtbot/factory_girl/blob/master/GETTING_STARTED.md
 FactoryGirl.define do
 
+  sequence :pnr_number do |n|
+    "XXX%03d" % n
+  end
+
+  sequence :first_name do |n|
+    names = ["ALEXEY", "NIKOLAI", "ELENA", "BORIS"]
+    names[n % names.size]
+  end
+
+  # если сделать другое их количество, то они пересекаться не будут!
+  sequence :last_name do |x|
+    last_names = ["IVANOV", "PETROV", "SKRIPCHENKO", "AVDEEV", "BARANOV"]
+    last_names[n % last_names.size]
+  end
+
   factory :order do
-    pnr_number {|x| "XXXX#{x}" }
+    pnr_number
+  end
+
+  factory :person do
+    first_name
+    last_name
+  end
+
+  factory :payture_charge do
+    order
+
+    # не работает! попробовать сделать его с помощью
+    # after_create do |payture_charge, proxy| ...
+
+    #factory :charged_payture_charge do
+    #  status 'charged'
+    #end
+  end
+
+  factory :payture_refund do
+    association :charge, :factory => :payture_charge
+  end
+
+  factory :cash_charge do
+    order
+  end
+
+  factory :cash_refund do
+    association :charge, :factory => :cash_charge
   end
 
 end
