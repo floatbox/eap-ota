95bf3b2359519ebe8a9e62328e469becb31afd07
codesnik <aronaxis@gmail.com>
2012-04-08 19:47:55 +0400

    ruby-1.9.3-head в deploy скрипте

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require "capistrano_colors"
 
 set :rvm_type, :system
 # закрепил версию, чтобы не прыгала в продакшне
-set :rvm_ruby_string, 'ruby-1.9.3-p125'
+set :rvm_ruby_string, 'ruby-1.9.3-head'
 
 require 'bundler/capistrano'
 
