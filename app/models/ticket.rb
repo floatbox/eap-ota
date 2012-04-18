3e1760ff199ad2fb771a42698d4eea818a88bef4
abragin <abragin@gmail.com>
2012-04-18 18:45:54 +0400

    fix: Ticket.sold теперь показывает и возвраты

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -44,7 +44,7 @@ class Ticket < ActiveRecord::Base
   before_save :recalculate_commissions, :set_validating_carrier
 
   scope :uncomplete, where(:ticketed_date => nil)
-  scope :sold, where(:status => ['ticketed', 'exchanged', 'returned'])
+  scope :sold, where(:status => ['ticketed', 'exchanged', 'returned', 'processed'])
 
   before_validation :set_refund_data, :if => lambda {kind == "refund"}
   before_validation :update_price_fare_and_add_parent, :if => lambda {parent_number}
