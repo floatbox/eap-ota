02b9ded1e9e8f4e8b01820c6f5683c5ce469d02e
abragin <abragin@gmail.com>
2012-04-25 18:21:38 +0400

    fix загрузки обменов

--- lib/amadeus/response/pnr_retrieve.rb
+++ lib/amadeus/response/pnr_retrieve.rb
@@ -87,7 +87,7 @@ module Amadeus
       end
 
       def parsed_exchange_string(s)
-        m = s.to_s.match(/(PAX|INF)?.*\/(\d+)-([\d-]+).{3}(-\d{3})?$/)
+        m = s.to_s.match(/(PAX|INF)?.*\/(\d{3})-([\d-]{10,14})(\w){3}([-\*]\w{3})?/)
         if m
           return({:number => m[3], :code => m[2], :inf => m[1]})
         end
