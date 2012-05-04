4ff30c1cabc00c0ddf1ae91f1ad2fd204f6ed7ea
Andrey Arefiev <arefiev@gmail.com>
2012-05-04 20:55:09 +0400

    существенно ускорил показ счетчиков направлений в админке

--- app/controllers/admin/counters_controller.rb
+++ app/controllers/admin/counters_controller.rb
@@ -11,20 +11,44 @@ class Admin::CountersController < Admin::BaseController
     @partners = Order.partners
     @data = []
     day_data = {}
-    total_s = 0
-    total_e = 0
+    @total_search = 0
+    @total_enter = 0
+    @total_partner = {}
     count = 2000
-    
+
+    @partners.each do |partner|
+      @total_partner[partner] = {} if @total_partner[partner].nil?
+      @total_partner[partner]['search'] = 0
+      @total_partner[partner]['enter'] = 0
+    end
+
     StatCounters.search_on_daterange(@date_condition, count).each do |day_direction|
       direction_index = day_direction['from'] + Destination.rts.invert[day_direction['rt']] + day_direction['to']
-      day_data[direction_index] = {} if day_data[direction_index].nil?
+      if day_data[direction_index].nil?
+        day_data[direction_index] = {}
+        day_data[direction_index]['search'] = {}
+        day_data[direction_index]['enter'] = {}
+        day_data[direction_index]['search']['api'] = {}
+        day_data[direction_index]['enter']['api'] = {}
+        @partners.each do |partner|
+          day_data[direction_index]['search']['api'][partner] = {}
+          day_data[direction_index]['enter']['api'][partner] = {}
+          day_data[direction_index]['search']['api'][partner]['total'] = 0
+          day_data[direction_index]['enter']['api'][partner]['total'] = 0
+        end
+      end
       day_data[direction_index]['search'] = StatCounters.deep_merge_sum(day_data[direction_index]['search'], day_direction['search'])
       day_data[direction_index]['enter'] = StatCounters.deep_merge_sum(day_data[direction_index]['enter'], day_direction['enter'])
-      total_s += day_direction.nested('search.api.total') || 0
-      total_e += day_direction.nested('enter.api.total') || 0
+      @total_search += day_direction['search']['api']['total'] if day_direction['search']
+      @total_enter += day_direction['enter']['api']['total'] if day_direction['enter']
+
+      @partners.each do |partner|
+        @total_partner[partner]['search'] += day_direction['search']['api'][partner]['total'] if day_direction['search'] && day_direction['search']['api'][partner]
+        @total_partner[partner]['enter'] += day_direction['enter']['api'][partner]['total'] if day_direction['enter'] && day_direction['enter']['api'][partner]
+      end
     end
-    @data = { :title => @date_range, :day_data => day_data, :total_s => total_s, :total_e => total_e }
-  
+    @partners.delete_if {|partner| @total_partner[partner]['search'].zero?}
+    @data = { :title => @date_range, :day_data => day_data }
   end
 
 end
