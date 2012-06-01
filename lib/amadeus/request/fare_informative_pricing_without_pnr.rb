cbfa6ac17f583c984e707e7bfd92c312055b78ab
waaa <peatrovich@gmail.com>
2012-06-01 19:47:17 +0400

    переделали для красоты

--- lib/amadeus/request/fare_informative_pricing_without_pnr.rb
+++ lib/amadeus/request/fare_informative_pricing_without_pnr.rb
@@ -1,8 +1,15 @@
 module Amadeus
   module Request
     class FareInformativePricingWithoutPNR < Amadeus::Request::Base
-      attr_accessor :people_count, :flights, :validating_carrier, :recommendation
+      attr_accessor :people_count, :recommendation
 
+      def flights
+        recommendation.journey.flights
+      end
+
+      def validating_carrier
+       recommendation.validating_carrier
+      end
     end
   end
 end
