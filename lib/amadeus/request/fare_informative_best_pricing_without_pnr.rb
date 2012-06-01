377d2969d2a0cb50fc58fac0f640233cf5a80aa2
waaa <peatrovich@gmail.com>
2012-06-01 21:34:06 +0400

    все наследуем

--- lib/amadeus/request/fare_informative_best_pricing_without_pnr.rb
+++ lib/amadeus/request/fare_informative_best_pricing_without_pnr.rb
@@ -1,15 +1,7 @@
 module Amadeus
   module Request
-    class FareInformativeBestPricingWithoutPNR < Amadeus::Request::Base
-      attr_accessor :people_count, :recommendation
+    class FareInformativeBestPricingWithoutPNR < FareInformativePricingWithoutPNR
 
-      def flights
-        recommendation.journey.flights
-      end
-
-      def validating_carrier
-        recommendation.validating_carrier_iata
-      end
     end
   end
 end
