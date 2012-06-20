b4e9bb323b4ee56cd98490551cbe803d09f7b707
waaa <peatrovich@gmail.com>
2012-06-20 18:40:39 +0400

    фигачим рекомендации массивами из informative_pricing

---
+++ spec/amadeus/fare_informative_best_pricing_without_pnr_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Amadeus::Response::FareInformativeBestPricingWithoutPNR do
+  context 'check the parser' do
+    let_once! :response do
+      amadeus_response('spec/amadeus/xml/Fare_InformativeBestPricingWithoutPNR_adults_complex.xml')
+    end
+
+    describe "response" do
+      subject { response }
+      it { should be_success }
+      its(:all_prices) { should  == [[4240.0, 4918.0]]}
+    end
+  end
+end
+
