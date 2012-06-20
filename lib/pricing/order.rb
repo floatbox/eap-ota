8ed29c4aea58fd07fdaa71a51f16b81b070bc6b3
abragin <abragin@gmail.com>
2012-06-20 20:04:18 +0400

    Округляем Order#expected_income

--- lib/pricing/order.rb
+++ lib/pricing/order.rb
@@ -43,7 +43,7 @@ module Pricing
     end
 
     def expected_income
-      expected_earnings - income_suppliers
+      (expected_earnings - income_suppliers).round(2)
     end
 
     # сумма данных по билетам. по идее, более точная информация, нежели сохраненная в заказе
