729a77871f720f43202f9cdd81be3b6feebe3ec3
waaa <peatrovich@gmail.com>
2012-06-21 19:16:33 +0400

    важный коммит! тест, в которым видно: всем ничего не ломается

--- spec/amadeus/fare_informative_pricing_without_pnr_spec.rb
+++ spec/amadeus/fare_informative_pricing_without_pnr_spec.rb
@@ -6,35 +6,15 @@ describe Amadeus::Response::FareInformativePricingWithoutPNR do
     Recommendation.example("MOWPAR PARMOW")
   end
 
-  describe 'lotz of recommendations for one adult & one child' do
-    let_once! :response do
-      amadeus_response('spec/amadeus/xml/Fare_InformativePricingWithoutPNR_lots_of_recommendations.xml')
-    end
-
-    subject {response}
-    specify { subject.recommendations(recommendation).count.should == 4}
-    specify { subject.recommendations(recommendation).first.price_fare.should == 64280}
-    specify { subject.recommendations(recommendation).first.booking_classes.should == ["Y", "Y", "Y", "Y"]}
-  end
-
-  describe 'simple one: 1 adult, 1 infant, 1 child' do
+  describe 'how we do not change the old order of things' do
     let_once! :response do
       amadeus_response('spec/amadeus/xml/Fare_InformativePricingWithoutPNR_all_of_human_groups.xml')
     end
-
     subject {response}
-    specify { subject.recommendations(recommendation).count.should == 1}
-    specify { subject.recommendations(recommendation).first.price_fare.should == 99640}
-    specify { subject.recommendations(recommendation).first.booking_classes.should == ["Y", "Y", "Y", "Y"]}
+
+    #Sic! They are the same!
+    specify {response.prices.should == [99640, 18086]}
+    specify {response.recommendations(recommendation).first.price_fare.should == 99640}
+    specify {response.recommendations(recommendation).first.price_tax.should == 18086}
   end
-
-  describe 'two adults' do
-      let_once! :response do
-        amadeus_response('spec/amadeus/xml/Fare_InformativePricingWithoutPNR_two_adults.xml')
-      end
-
-      subject {response}
-      specify { subject.recommendations(recommendation).count.should == 1}
-      specify { subject.recommendations(recommendation).first.price_fare.should == 112560}
-    end
 end
