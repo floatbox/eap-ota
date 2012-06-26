fb5d3e2d2d38668145ad48fabc07b9b43acc280f
Nick Zayarny <nickzayarny@gmail.com>
2012-06-27 02:34:33 +0400

    опечатка по AB

--- lib/commission.rb
+++ lib/commission.rb
@@ -300,7 +300,7 @@ commission "0%/0%"
 carrier "AB", "AIR BERLIN"
 ########################################
 
-example 'cdgsvo svocdgh'
+example 'cdgsvo svocdg/lh'
 example 'svocdg'
 agent    "5% по всем направлениям через DTT"
 subagent "3% по всем направлениям через DTT"
