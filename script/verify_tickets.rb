d00891cd0e42a676c7de5a2180c4d1cecc70b546
waaa <peatrovich@gmail.com>
2012-07-03 19:11:23 +0400

    забыла каменты к скрипту

--- script/verify_tickets.rb
+++ script/verify_tickets.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../../config/environment',  __FILE__)
 
 def orders
+  #использую last_pay_time потому, что он сейчас обновляется руками и актуальней last_tkt_date
+  #пустой pnr может быть, в частности, в случае доплаты, которую мы не трогаем
   Order.where("pnr_number != ? AND ticket_status = ? AND last_pay_time < ?", "", "booked", Time.now)
 end
 
