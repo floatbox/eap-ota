e9789f2b60f2d2733c4451a7f981f843a8f4e1f4
waaa <peatrovich@gmail.com>
2012-07-03 19:04:51 +0400

    проверяющий бронь скрипт стал более лучше продуман

--- script/verify_tickets.rb
+++ script/verify_tickets.rb
@@ -1,17 +1,20 @@
 require File.expand_path('../../config/environment',  __FILE__)
 
-def select_orders
-  Order.where("ticket_status = ? AND last_pay_time < ? AND payment_type = ?", "booked", Date.tomorrow, "cash").each do |order|
-    yield order
+def orders
+  Order.where("pnr_number != ? AND ticket_status = ? AND last_pay_time < ?", "", "booked", Time.now)
+end
+
+def verify_status
+  orders.each do |order|
+    begin
+      response = ::Amadeus.booking{|a| a.pnr_retrieve(:number => order.pnr_number)}
+      if response && response.flights_hash.blank?
+        order.update_attributes(:ticket_status => 'canceled')
+        puts "Amadeus canceled this booking thus we cancel it too: #{order.pnr_number}"
+      end
+    rescue
+      puts "Error: #{$!} pnr number: #{order.pnr_number}"
+    end
   end
 end
-
-public
-def verify_status
- unless ::Amadeus.booking{|a| a.pnr_retrieve(:number => self.pnr_number).flights_hash.present?}
-   self.update_attributes(:ticket_status => 'canceled')
-   #puts "#{self.pnr_number} canceled"
- end
-end
-
-select_orders(&:verify_status)
+verify_status
