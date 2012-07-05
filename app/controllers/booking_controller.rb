178c233211799017541eecdd8ffbdc98f8ce1771
waaa <peatrovich@gmail.com>
2012-07-05 19:52:17 +0400

    добавила обработку ошибок в api_redirect

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -85,6 +85,10 @@ class BookingController < ApplicationController
     else
       redirect_to "#{Conf.api.url_base}/"
     end
+  rescue IataStash::NotFound => iata_error
+    render 'api/yandex_failure', :status => 404, :locals => {:message => iata_error.message}
+  rescue ArgumentError => argument_error
+    render 'api/yandex_failure', :status => 400, :locals => {:message => argument_error.message}
   ensure
     StatCounters.inc %W[enter.api_redirect.total]
   end
