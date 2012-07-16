eab830dc20b3687810fcbbd48715650ad373c776
waaa <peatrovich@gmail.com>
2012-07-16 20:53:31 +0400

    третий вариант fare_informative парсера, используем id пассажиров

--- lib/amadeus/response/fare_informative_pricing_without_pnr.rb
+++ lib/amadeus/response/fare_informative_pricing_without_pnr.rb
@@ -10,11 +10,8 @@ module Amadeus
         xpath('//r:pricingGroupLevelGroup').each do |pg|
           passengers_in_group = pg.xpath('r:numberOfPax/r:segmentControlDetails/r:numberOfUnits').to_i
           price_total += pg.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="712"][r:currency="RUB"]/r:amount').to_f * passengers_in_group
-          # FIXME сделать один xpath
-          price_fare += (
-            pg.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="E"][r:currency="RUB"]/r:amount').to_f ||
-            pg.xpath('r:fareInfoGroup/r:fareAmount/r:monetaryDetails[r:typeQualifier="B"][r:currency="RUB"]/r:amount').to_f
-          ) * passengers_in_group
+          price_fare += pg.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="E"][r:currency="RUB"]/r:amount|
+          r:fareInfoGroup/r:fareAmount/r:monetaryDetails[r:typeQualifier="B"][r:currency="RUB"]/r:amount').to_f * passengers_in_group
         end
 
         # FIXME не очень надежный признак
@@ -28,47 +25,26 @@ module Amadeus
       def local_prices pricing_group
         passengers_in_group = pricing_group.xpath('r:numberOfPax/r:segmentControlDetails/r:numberOfUnits').to_i
           price_total = pricing_group.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="712"][r:currency="RUB"]/r:amount').to_f * passengers_in_group
-          # FIXME сделать один xpath
-          price_fare = (
-            pricing_group.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="E"][r:currency="RUB"]/r:amount').to_f ||
-            pricing_group.xpath('r:fareInfoGroup/r:fareAmount/r:monetaryDetails[r:typeQualifier="B"][r:currency="RUB"]/r:amount').to_f
-          ) * passengers_in_group
+          price_fare = pricing_group.xpath('r:fareInfoGroup/r:fareAmount/r:otherMonetaryDetails[r:typeQualifier="E"][r:currency="RUB"]/r:amount|
+          r:fareInfoGroup/r:fareAmount/r:monetaryDetails[r:typeQualifier="B"][r:currency="RUB"]/r:amount').to_f * passengers_in_group
           price_tax = price_total - price_fare
           [price_fare, price_tax]
       end
 
       def recommendations rec
-        good_for_transposing = []
-        max_count = [adults, infants, children].map(&:count).max
+        xml_recommendations = xpath("//r:pricingGroupLevelGroup").group_by do |pr|
+          pr.xpath("r:passengersID/r:travellerDetails/r:measurementValue").to_s
+        end.values
+        min_count = xml_recommendations.map(&:count).min
+        xml_recommendations = xml_recommendations.each{|values| values[min_count..-1] = []}
 
-        [adults, infants, children].each do |age_group|
-          age_group.fill( age_group.first, (age_group.count...max_count)) if age_group.count < max_count
-          good_for_transposing << age_group if age_group.present?
-        end
-
-        good_for_transposing.transpose.map do |complex_group|
-          complex_group.compact!
+        xml_recommendations.transpose.map do |xml_recommendation|
           new_rec = Recommendation.new(:variants => rec.variants)
-          new_rec.booking_classes = complex_group.first.xpath('r:fareInfoGroup/r:segmentLevelGroup/r:cabinGroup/r:cabinSegment/r:bookingClassDetails/r:designator').map(&:to_s)
-          new_rec.cabins = complex_group.first.xpath('r:fareInfoGroup/r:segmentLevelGroup/r:cabinGroup/r:cabinSegment/r:bookingClassDetails/r:option').map(&:to_s)
-          new_rec.price_fare, new_rec.price_tax = complex_group.map{|cg|local_prices cg}.transpose.map{|x| x.reduce(:+)}
+          new_rec.booking_classes = xml_recommendation.first.xpath('r:fareInfoGroup/r:segmentLevelGroup/r:cabinGroup/r:cabinSegment/r:bookingClassDetails/r:designator').map(&:to_s)
+          new_rec.cabins = xml_recommendation.first.xpath('r:fareInfoGroup/r:segmentLevelGroup/r:cabinGroup/r:cabinSegment/r:bookingClassDetails/r:option').map(&:to_s)
+          new_rec.price_fare, new_rec.price_tax = xml_recommendation.map{|cg|local_prices cg}.transpose.map{|x| x.reduce(:+)}
           new_rec
         end
-      end
-
-      #массив pricing_groups, соответствующих только взрослым
-      def adults
-        xpath("//r:pricingGroupLevelGroup[r:fareInfoGroup/r:segmentLevelGroup/r:ptcSegment/r:quantityDetails/r:unitQualifier='ADT']")
-      end
-
-      #массив pricing_groups, соответствующих только детям
-      def children
-        xpath("//r:pricingGroupLevelGroup[r:fareInfoGroup/r:segmentLevelGroup/r:ptcSegment/r:quantityDetails/r:unitQualifier='CH']")
-      end
-
-      #массив pricing_groups, соответствующих только младенцам без места
-      def infants
-        xpath("//r:pricingGroupLevelGroup[r:fareInfoGroup/r:segmentLevelGroup/r:ptcSegment/r:quantityDetails/r:unitQualifier='IN']")
       end
     end
   end
