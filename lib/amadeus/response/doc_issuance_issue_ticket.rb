f44fe41a56928b01e8741002b601bd1bc6297ecd
abragin <abragin@gmail.com>
2012-07-18 17:19:14 +0400

    Тестовая выписка в авиацентре из админки

--- lib/amadeus/response/doc_issuance_issue_ticket.rb
+++ lib/amadeus/response/doc_issuance_issue_ticket.rb
@@ -3,11 +3,11 @@ module Amadeus
   module Response
     class DocIssuanceIssueTicket < Amadeus::Response::Base
       def success?
-        xpath('r:processingStatus/r:statusCode').to_s == 'O'
+        xpath('//r:processingStatus/r:statusCode').to_s == 'O'
       end
 
       def error_message
-        xpath('r:errorGroup/r:errorWarningDescription/r:freeText').to_s.try(:strip)
+        xpath('//r:errorGroup/r:errorWarningDescription/r:freeText').to_s.try(:strip)
       end
     end
   end
