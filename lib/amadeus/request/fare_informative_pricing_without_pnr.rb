3ad5b80df272ae61b5e7a92d0b2f9d34edb30440
waaa <peatrovich@gmail.com>
2012-08-01 17:03:30 +0400

    booking class override for informative pricing request

--- lib/amadeus/request/fare_informative_pricing_without_pnr.rb
+++ lib/amadeus/request/fare_informative_pricing_without_pnr.rb
@@ -1,7 +1,7 @@
 module Amadeus
   module Request
     class FareInformativePricingWithoutPNR < Amadeus::Request::Base
-      attr_accessor :people_count, :recommendation
+      attr_accessor :people_count, :recommendation, :booking_class
 
       def flights
         recommendation.journey.flights
@@ -9,6 +9,12 @@ module Amadeus
 
       def validating_carrier
        recommendation.validating_carrier_iata
+      end
+
+      private
+
+      def booking_class_for_flight fl
+        booking_class || recommendation.booking_class_for_flight(fl)
       end
     end
   end
