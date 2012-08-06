be4073c2d625d97a3cd854d6e56488fb4999af7a
Andrey Arefiev <arefiev@gmail.com>
2012-08-06 20:00:44 +0400

    посылаем нотисы через delayed_job

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,9 +5,10 @@ class Notification < ActiveRecord::Base
   belongs_to :order
   belongs_to :typus_user
 
-  validates :destination, :presence => true
-
-  before_validation :set_order_data
+  #validates :destination, :presence => true
+  #before_validation :set_order_data
+  before_create :set_order_data
+  after_create :delayed_send
 
   scope :email_queue, where(
     :method => 'email',
@@ -21,8 +22,60 @@ class Notification < ActiveRecord::Base
   alias_attribute :email, :destination
 
   def self.statuses; ["sent", "error", "delayed"] end
-  def self.formats; ["booking", "ticket"] end
+  def self.formats; ["order", "booking", "ticket"] end
   def self.langs; ["EN"] end
+
+  # создание разных типов нотификаций
+  # дефолтный нотис при создании ордера
+  def create_notice
+    self.status = 'delayed'
+    if save
+      order.queued_email!
+      self.delay(queue: 'notification', run_at: 10  .minutes.from_now, priority: 5).send_notice
+    end
+  end
+
+  def set_order_data
+    self.pnr_number = order.pnr_number
+    self.destination = order.email
+  end
+
+  def delayed_send
+    self.delay(queue: 'notification', run_at: 2.minutes.from_now, priority: 5).send_notice if status.blank?
+  end
+
+  def prepare_notice
+    if format.blank?
+      self.format = order.send_notice_as
+    end
+
+    eng = true unless lang.blank?
+    if subject.blank?
+      case format
+        when 'ticket'
+          self.subject = eng ? "Your E-ticket (booking #{pnr_number})" : "Ваш электронный билет #{pnr_number}" 
+        when 'order'
+          self.subject = eng ? "Your Order #{pnr_number})" : "Ваш заказ на Eviterra.com #{pnr_number}"
+        when 'booking'
+          self.subject = eng ? "Your Booking #{pnr_number})" : "Ваше бронирование #{pnr_number}"
+      end
+    end
+    save
+  end
+
+  def send_notice
+    prepare_notice
+    logger.info 'Notification: sending email'
+    NotifierMailer.notice(self).deliver
+    update_attributes({:status => 'sent', 'sent_at' => Time.now})
+    order.update_email_status(format + '_sent')
+    puts "Email pnr #{pnr_number} to #{destination} SENT on #{Time.now}"
+    rescue
+      reload
+      update_attribute(:status, 'error')
+      puts "Email pnr #{pnr_number} to #{destination} ERROR on #{Time.now}"
+    raise
+  end
 
   def create_pnr
     self.attach_pnr = true
