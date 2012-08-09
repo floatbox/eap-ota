4d48db650f22f744b837c4a9dc8ee9bdec7e0940
waaa <peatrovich@gmail.com>
2012-08-09 17:12:04 +0400

    небольшие фиксы

--- app/helpers/insurance_helper.rb
+++ app/helpers/insurance_helper.rb
@@ -3,16 +3,16 @@ module InsuranceHelper
 
   def smart_insurance_uri order_form
     @order_form = order_form
-    uri = "https://www.smart-ins.ru/vzr_iframe/light?#{params.to_query}" if params
+    uri = "https://www.smart-ins.ru/vzr_iframe/light?#{uri_params.to_query}" if uri_params
   end
 
   private
 
-  def params
+  def uri_params
     journey = @order_form.recommendation.journey
     start_date = convert_date(journey.flights.first.departure_date)
     { :start_date => start_date,
-      :end_date => @order_form.recommendation.rt ? convert_date(journey.flights.last.arrival_date) : (Date.parse(start_date) + 30.days).strftime('%d.%m.%Y'),
+      :end_date => (@order_form.recommendation.rt ? convert_date(journey.flights.last.arrival_date) : (Date.parse(start_date) + 30.days).strftime('%d.%m.%Y')),
       :country => @order_form.clean_route.second.country.alpha2,
       :email => @order_form.email,
       :phone => @order_form.phone,
@@ -30,8 +30,8 @@ module InsuranceHelper
         'name' => person.last_name,
         'dob' => person.birthday.strftime('%d.%m.%Y'),
         'sex' => (person.sex == 'f' ? 1 : 0),
-        'passport1' => person.passport[0..1],
-        'passport2' => person.passport[2..-1] }
+        'passport1' => person.cleared_passport[0..1],
+        'passport2' => person.cleared_passport[2..-1] }
     end
     buyers
   end
