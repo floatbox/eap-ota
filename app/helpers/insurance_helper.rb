d69af40eabb6f6de2e2fe8ecddb3e555f98488ef
waaa <peatrovich@gmail.com>
2012-08-09 19:13:12 +0400

    insurance fix

--- app/helpers/insurance_helper.rb
+++ app/helpers/insurance_helper.rb
@@ -2,34 +2,32 @@
 module InsuranceHelper
 
   def smart_insurance_uri order_form
-    @order_form = order_form
+    uri_params = insurance_uri_params(order_form)
     uri = "https://www.smart-ins.ru/vzr_iframe/light?#{uri_params.to_query}" if uri_params
   end
 
-  private
-
-  def uri_params
-    journey = @order_form.recommendation.journey
-    start_date = convert_date(journey.flights.first.departure_date)
+  def insurance_uri_params order_form
+    journey = order_form.recommendation.journey
+    start_date = smart_insurance_date(journey.flights.first.departure_date)
     insurance_end_date = if journey.segments.count > 1
-      convert_date(journey.flights.last.arrival_date)
+      smart_insurance_date(journey.flights.last.arrival_date)
     else
       (Date.parse(start_date) + 30.days).strftime('%d.%m.%Y')
     end
     { :start_date => start_date,
       :end_date => insurance_end_date,
-      :country => @order_form.recommendation.variants.first.segments.first.flights.last.arrival.country.alpha2,
-      :email => @order_form.email,
-      :phone => @order_form.phone,
+      :country => journey.segments.first.flights.last.arrival.country.alpha2,
+      :email => order_form.email,
+      :phone => order_form.phone,
       :city => journey.flights.first.departure.city.name_ru,
-      :buyers => buyers,
+      :buyers => insurance_buyers(order_form.people_by_age),
       :partner => 'eviterra'
     }
   end
 
-  def buyers
+  def insurance_buyers people
     buyers = {}
-    @order_form.people_by_age.each_with_index do |person, index|
+    people.each_with_index do |person, index|
       buyers[index.to_s] = {
         'surname' => person.first_name,
         'name' => person.last_name,
@@ -41,7 +39,7 @@ module InsuranceHelper
     buyers
   end
 
-  def convert_date str
+  def smart_insurance_date str
     Date.parse(str.gsub(/^(\d\d)(\d\d?)(\d\d?)$/){"%02d.%02d.%02d" % [$3, $2, $1].map(&:to_i)}).strftime('%d.%m.%Y')
   end
 end
