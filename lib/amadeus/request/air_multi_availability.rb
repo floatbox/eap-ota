16d21f44bba017ae257b342433b0077040781e70
abragin <abragin@gmail.com>
2012-08-15 20:48:34 +0400

    cabin в air_multi_availability

--- lib/amadeus/request/air_multi_availability.rb
+++ lib/amadeus/request/air_multi_availability.rb
@@ -1,7 +1,11 @@
 module Amadeus
   module Request
     class AirMultiAvailability < Amadeus::Request::Base
-      attr_accessor :flight
+      attr_accessor :flight, :cabin
+
+      def converted_cabin
+        {Y: 3, M: 3, W: 3, C: 2, F: 1}[cabin.to_sym] if cabin
+      end
     end
   end
 end
