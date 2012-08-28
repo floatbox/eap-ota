008732c12e19ce964f0c4b09c5b4735f111afbb7
waaa <peatrovich@gmail.com>
2012-08-28 20:59:40 +0400

    метод чтобы брать банк на конкретную дату(гем money)

--- lib/cbr.rb
+++ lib/cbr.rb
@@ -4,5 +4,12 @@ module CBR
       course = Conf.cbr.usd[Date.today] || Conf.cbr.usd.max[1]
       (amount * course).round(2)
     end
+
+    # bank.exchange(money, "USD")
+    def bank date
+      bank = VariableExchangeBank.new.add_rate("RUB", "USD", Conf.cbr.usd[date])
+      bank.add_rate("USD", "RUB", (1/Conf.cbr.usd[date]))
+      bank
+    end
   end
 end
