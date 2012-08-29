5e237e0fc8ee5e345e1e0cf731dcd44a92e9eb89
waaa <peatrovich@gmail.com>
2012-08-29 18:51:29 +0400

    юзаем правильный money

--- lib/cbr.rb
+++ lib/cbr.rb
@@ -5,9 +5,10 @@ module CBR
       (amount * course).round(2)
     end
 
-    # bank.exchange(money, "USD")
+    # https://github.com/RubyMoney/money
+    # bank.exchange_with(money, "USD")
     def bank date
-      bank = VariableExchangeBank.new
+      bank =  Money::Bank::VariableExchange.new
       bank.add_rate("RUB", "USD", Conf.cbr.usd[date])
       bank.add_rate("USD", "RUB", (1/Conf.cbr.usd[date]))
       bank
