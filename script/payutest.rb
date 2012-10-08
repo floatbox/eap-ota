cf3ed6f9563b2393001fccd7ee06d81313523da0
codesnik <aronaxis@gmail.com>
2012-10-08 20:18:41 +0400

    обновил script/payutest.rb

--- script/payutest.rb
+++ script/payutest.rb
@@ -1,11 +1,16 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -Ilib
 
+require_relative '../config/application'
+require 'conf'
 require 'pp'
 # чтобы не грузить весь environment:
-class Rails
+module Rails
   def self.logger; nil; end
 end
-require_relative '../lib/payu'
+require 'payu'
+require 'active_model'
+require 'key_value_init'
+require 'credit_card'
 
 
 require 'logger'
@@ -14,17 +19,23 @@ Payu.logger = Logger.new(STDOUT)
 payu = Payu.new(
   merchant: 'EVITERRA',
   host: 'sandbox8ru.epayment.ro',
+  # host: 'secure.payu.ru',
+  # ssl: true,
   seller_key: 'w7I2R8~V7=dm5H7[r1k5'
 )
 
-
 order_id = Time.now.strftime('test_%y%m%d_%H%M%S')
-response = payu.block 123, nil, :order_id => order_id
 
-if response.success?
+card = Payu.test_card
+response = payu.block 123, card, :order_id => order_id
+
+puts response.inspect
+if response.threeds?
+  puts response.threeds_url
+end
 
 #  unblock_response = payu.unblock(:order_id => response.ref)
 
-  charge_response = payu.charge(:order_id => response.ref)
+#  charge_response = payu.charge(:order_id => response.ref)
 
-end
+#end
