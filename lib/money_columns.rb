26518d12a7cb918e0f97315f276aca987e682f0a
waaa <peatrovich@gmail.com>
2012-10-17 20:25:21 +0400

    во имя аккуратности

--- lib/money_columns.rb
+++ lib/money_columns.rb
@@ -18,7 +18,7 @@ module MoneyColumns
     attr_helpers = attrs.map{ |name|(name.to_s+'_as_string').to_sym }
     [attrs, attr_helpers].transpose.each do |attr, attr_helper|
       define_method "#{attr_helper}" do
-        instance_variable_get"@#{attr_helper}" || method(attr.to_sym).call.try(&:with_currency)
+        instance_variable_get"@#{attr_helper}" || method(attr).call.try(&:with_currency)
       end
       define_method "#{attr_helper}=" do |value|
         instance_variable_set "@#{attr_helper}", value
