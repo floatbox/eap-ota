5afd8e46b5686a007db306abca7997ee1313e971
Andrey Arefiev <arefiev@gmail.com>
2012-12-13 18:28:35 +0400

    графики для дашборда

--- app/assets/javascripts/typus/custom.js
+++ app/assets/javascripts/typus/custom.js
@@ -1,3 +1,7 @@
 //= require ./replace-with
 //= require ./jquery.timeago.js
 //= require ./jquery.timeago.ru.js
+
+//= require jquery.flot
+//= require jquery.flot.resize
+//= require jquery.flot.pie
