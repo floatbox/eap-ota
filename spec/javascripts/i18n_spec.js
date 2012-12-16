acbd6d3fefbe903ba17e661e3eed2efbb4626cb2
codesnik <aronaxis@gmail.com>
2012-12-16 19:00:45 +0400

    чище спеки

--- spec/javascripts/i18n_spec.js
+++ spec/javascripts/i18n_spec.js
@@ -22,22 +22,28 @@ describe('russian i18n', function() {
 
   describe('pluralization', function() {
 
-    it("should pluralize russian messages for count 2", function() {
+    beforeEach(function() {
       I18n.locale = 'ru'
+    })
+
+    it("should pluralize russian messages for count 2", function() {
       expect( I18n.t('errors.messages.too_long', {count: 2}) )
         .toEqual('слишком большой длины (не может быть больше чем 2 символа)')
     })
 
     it("should pluralize russian messages for count 0", function() {
-      I18n.locale = 'ru'
       expect( I18n.t('errors.messages.too_long', {count: 0}) )
         .toEqual('слишком большой длины (не может быть больше чем 0 символов)')
     })
 
     it("should pluralize russian messages for count 21", function() {
-      I18n.locale = 'ru'
       expect( I18n.t('errors.messages.too_long', {count: 21}) )
         .toEqual('слишком большой длины (не может быть больше чем 21 символ)')
+    })
+
+    it("should pluralize russian messages using scope", function() {
+      expect( I18n.t('too_long', {scope: 'errors.messages', count: 2}) )
+        .toEqual('слишком большой длины (не может быть больше чем 2 символа)')
     })
 
   })
