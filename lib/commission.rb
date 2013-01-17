0a53a0532850cb74e51760c08bd0fdc94617f42d
inque <me@inque.ru>
2013-01-17 23:24:56 +0400

    disable 7D

--- lib/commission.rb
+++ lib/commission.rb
@@ -425,19 +425,21 @@ no_commission
 carrier "7D", "DONBASSAERO"
 ########################################
 
-example 'svocdg'
+#example 'svocdg'
 strt_date "11.04.2011"
 agent "С 11.04.11г. 5 (Пять) % от всех опубликованных тарифов на собственные рейсы авиакомпании DONBASSAERO AIRLINES (LLC) (7D/897);"
 subagent "С 11.04.11г. 3,5% от всех опубл. тарифов на собств. рейсы 7D;"
 discount "2%"
+disabled "out of BSP"
 commission "5%/3.5%"
 
-example 'cdgsvo svocdg/ab'
+#example 'cdgsvo svocdg/ab'
 strt_date "11.04.2011"
 agent "С 11.04.11г. 5 (Пять) % от всех опубликованных тарифов на интерлайн-перевозки как с участием собственных, так и без участия собственных рейсов (только рейсы интерлайн-партнёров) авиакомпании DONBASSAERO AIRLINES (LLC) (7D/897);"
 subagent "С 11.04.11г. 3,5% от всех опубл. тарифов на рейсы Interline с уч. собств. рейсов 7D;"
 interline :yes, :absent
 discount "2%"
+disabled "out of BSP"
 commission "5%/3.5%"
 
 carrier "7W", "WINDROSE"
