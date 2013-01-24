0c836d02c78399e3c6badf4efd64d890f7311a4b
codesnik <aronaxis@gmail.com>
2013-01-24 14:12:08 +0400

    unicorn dir

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,8 @@
+project_home = ENV['UNICORN_HOME'] || '.'
+working_directory project_dir
+
 listen  (ENV['UNICORN_PORT'] || 8001).to_i, backlog: (ENV['UNICORN_BACKLOG'] || 100).to_i
 
-working_directory File.expand_path('../..', __FILE__)
 
 # What the timeout for killing busy workers is, in seconds
 timeout (ENV['UNICORN_TIMEOUT'] || 60).to_i
@@ -19,7 +21,7 @@ worker_processes (ENV['UNICORN_WORKERS'] || 1).to_i
 # Where to drop a pidfile
 # pid '/home/rack/eviterra/shared/pids/unicorn.pid'
 # TODO
-pid 'tmp/pids/unicorn.pid'
+pid project_home + '/tmp/pids/unicorn.pid'
 
 before_fork do |server,worker|
   if defined?(ActiveRecord::Base)
