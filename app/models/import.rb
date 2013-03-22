15a4799a036a7e9fd46d4efc9085738111624b2d
codesnik <aronaxis@gmail.com>
2013-03-22 22:09:20 +0700

    помощь выгрузке

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,9 +1,15 @@
 # encoding: utf-8
 require 'digest/md5'
+require 'pathname'
 class Import < ActiveRecord::Base
 
-  def self.from_file(filepath, kind)
-    filename = File.basename(filepath)
+  def self.from_file(kind, filepath, basedir=nil)
+    filename =
+      if basedir
+        Pathname(filepath).relative_path_from( Pathname(basedir) ).to_s
+      else
+        File.basename(filepath)
+      end
     content = File.open(filepath, 'r:binary') { |f| f.read }
     md5 = Digest::MD5.hexdigest(content)
     find_or_create_by_md5(md5, kind: kind, content: content, filename: filename)
@@ -36,14 +42,9 @@ class Import < ActiveRecord::Base
   def process!
     parse.each do |row|
       kind, pan, amount, commission, charged_on = row.values_at(:kind, :pan, :amount, :commission, :charged_on)
-      klass =
-        case kind
-        when :charge
-          PaytureCharge
-        when :refund
-          PaytureRefund
-        end
-      condition = klass.where(pan: pan, amount: amount, charged_on: charged_on)
+
+      klass = Payment.select_class(:payture, kind)
+      condition = klass.by_pan(row[:pan]).secured.where(price: row[:price])
       if condition.count > 1
         logger.warn "several payments are matching for for pan: #{pan}, amount: #{amount}, charged_on: #{charged_on}"
         next
