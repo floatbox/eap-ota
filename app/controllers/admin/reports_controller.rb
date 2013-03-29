99ba09fd4abf25915dc5ddb02a49d427c0ccd801
Andrey Arefiev <arefiev@gmail.com>
2013-03-29 17:30:59 +0400

    строка Заказ/Успех в отчетах

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -88,14 +88,15 @@ class Admin::ReportsController < Admin::BaseController
         partner[:income_total] = partner[:orders] && partner[:orders].income_total ? partner[:orders].income_total : 0
         partner[:income_share] = partner[:income_total] ? partner[:income_total].to_f / data[:orders].income_total.to_f * 100 : 0
         partner[:conv] = partner[:enter] && !partner[:enter].zero?  ? (partner[:order_count] / partner[:enter].to_f) * 100 : 0
-        partner[:enteres_per_search] = partner[:search] && !partner[:search].zero? && partner[:enter] ? (partner[:enter] / partner[:search].to_f) * 100 : 0
+        partner[:enters_per_search] = partner[:search] && !partner[:search].zero? && partner[:enter] ? (partner[:enter] / partner[:search].to_f) * 100 : 0
         partner[:markup] = !partner[:order_total].zero? ? (partner[:income_total] / partner[:order_total]) * 100 : 0
       end
 
       data[:orders_per_search] = !data[:searches].zero? ? data[:orders].order_count.to_f / data[:searches].to_f  * 100 : 0
-      data[:enteres_per_search] = !data[:searches].zero? ? data[:enter].to_f / data[:searches].to_f  * 100 : 0
+      data[:enters_per_search] = !data[:searches].zero? ? data[:enter].to_f / data[:searches].to_f  * 100 : 0
       data[:successes_per_enter] = !data[:enter].zero? ? data[:enter_success].to_f / data[:enter].to_f  * 100 : 0
       data[:conv] = !data[:enter].zero? ? data[:orders].order_count.to_f / data[:enter].to_f  * 100 : 0
+      data[:conv_success] = !data[:enter_success].zero? ? data[:orders].order_count.to_f / data[:enter_success].to_f  * 100 : 0
 
       @report << data
     end
