7f0c3e7fc2e4a9801537243021c75aabf207382a
abragin <abragin@gmail.com>
2013-04-01 13:01:58 +0400

    Удалил ненужные миграции

--- db/migrate/20130315162139_add_fee_calculation_details_to_orders_and_tickets.rb
+++
@@ -1,6 +0,0 @@
-class AddFeeCalculationDetailsToOrdersAndTickets < ActiveRecord::Migration
-  def change
-    add_column :tickets, :fee_calculation_details, :text, :default => '', :null => false
-    add_column :orders, :fee_calculation_details, :text, :default => '', :null => false
-  end
-end
--- db/migrate/20130328171157_drop_fee_scheme_from_tickets.rb
+++
@@ -1,9 +0,0 @@
-class DropFeeSchemeFromTickets < ActiveRecord::Migration
-  def up
-    remove_column :tickets, :fee_scheme
-  end
-
-  def down
-    add_column :tickets, :fee_scheme, :string, :default => ''
-  end
-end
