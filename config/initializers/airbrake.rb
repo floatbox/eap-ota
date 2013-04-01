d645b20cbefca6126b508c2c91b1997a2f1af1e6
codesnik <aronaxis@gmail.com>
2013-04-01 18:12:41 +0400

    юзеринфо в айрбрейк нотификациях

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = 'a4ca32b7fb71874b9f4c5f38ff2c6da2'
+  config.user_attributes = [:email]
+  # раскомментировать, чтобы дебажить airbrake в development mode
+  # config.development_environments = []
 end
