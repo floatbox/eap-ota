4b4e38fcf1c9b99a4777bfbcaf6c2830a659ac37
codesnik <aronaxis@gmail.com>
2013-04-01 18:52:01 +0400

    airbrake javascript notifications

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = 'a4ca32b7fb71874b9f4c5f38ff2c6da2'
+  config.js_api_key = '2b98a8cf6039754404a090dc00a745f5'
   config.user_attributes = [:email]
-  # раскомментировать, чтобы дебажить airbrake в development mode
+  # раскомментировать, чтобы дебажить airbrake не на продакшне
   # config.development_environments = []
 end
