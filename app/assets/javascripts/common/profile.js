3c6d8e12971a9093cbd0a08ec9b08f0cfa7ab166
Maxim Zakharov <mszakharov@gmail.com>
2013-04-10 20:50:58 +0400

    Добавил в попап формы восстановления пароля и регистрации.

--- app/assets/javascripts/common/profile.js
+++ app/assets/javascripts/common/profile.js
@@ -17,7 +17,7 @@ initPopup: function() {
         if (empty !== this.empty) {
             $(this).prev('label').toggle(this.empty = empty);
         }
-    });
+    }).trigger('input');
     this.el.on('click', function(event) {
         event.stopPropagation();
     });
@@ -27,6 +27,31 @@ initPopup: function() {
     this._hide = function(event) {
         if (event.type == 'click' || event.which == 27) that.hide();
     };
+    this.slider = this.el.find('.phu-slider');
+    this.el.find('.phu-forgot-link').on('click', function(event) {
+        event.preventDefault();
+        var le = $('#signin-email');
+        var re = $('#forgot-email');
+        re.val(le.val()).trigger('input');
+        that.el.find('.phu-forgot').show();
+        that.slider.animate({left: -260}, 200, function() {
+            re.focus();
+        });
+    });
+    this.el.find('.phu-remember-link').on('click', function(event) {
+        event.preventDefault();
+        var password = $('#customer_password').val('').trigger('input');
+        that.slider.animate({left: 0}, 200, function() {
+            password.focus();
+            that.el.find('.phu-forgot').hide();
+        });
+    });
+    this.el.find('.phu-signup-link').on('click', function(event) {
+        event.preventDefault();
+        that.el.find('.phu-signup').slideDown(100, function() {
+            $('#signup-email').focus();
+        });
+    });
 },
 show: function() {
     var that = this;
@@ -37,6 +62,7 @@ show: function() {
 },
 hide: function() {
     this.el.removeClass('phu-active');
+    this.el.find('.phu-signup').hide();
     $w.off('click keydown', this._hide);
 }
 };
