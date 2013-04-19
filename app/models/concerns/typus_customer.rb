869653723317531939cbc50d0728e4a52e369951
Andrey Arefiev <arefiev@gmail.com>
2013-04-19 16:51:23 +0400

    конфирм ссылка и прочее про кастомеров в админке

--- app/models/concerns/typus_customer.rb
+++ app/models/concerns/typus_customer.rb
@@ -5,7 +5,10 @@ module TypusCustomer
 
 
   def orders_count_link
-    "<a href='/admin/orders?customer_id=#{id}'>Заказов: #{orders.size}</a>".html_safe    
+    "<a href='/admin/orders?customer_id=#{id}'>Заказов: #{orders.size}</a>".html_safe
   end
   
+  def confirm_link
+    "<a href='/profile/verification?confirmation_token=#{confirmation_token}'>#{confirmation_token}</a>".html_safe if !confirmation_token.blank?
+  end
 end
