5012b4ba6d178ba0ef92863508a85812a952f70b
codesnik <aronaxis@gmail.com>
2013-05-04 04:51:30 +0400

    фикс в хелпере

--- app/helpers/debug_helper.rb
+++ app/helpers/debug_helper.rb
@@ -1,7 +1,9 @@
 module DebugHelper
   def debug_service_env(service)
     conf = Conf.send(service)
-    status = conf.enabled ? conf.env : 'disabled'
+    # убрал, для большинства сервисов enabled сейчас не используется
+    #status = conf.enabled ? conf.env : 'disabled'
+    status = conf.env
     if status != Rails.env
       "<span style='color:red'>#{status}</span>".html_safe
     else
