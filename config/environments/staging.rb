48f07e338966ce526bcf8f54cb9b561924d066cc
Andrey Arefiev <arefiev@gmail.com>
2013-05-07 20:26:09 +0400

    правильная настройка мейлера

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,6 +4,5 @@ require File.expand_path('../production.rb', __FILE__)
 Eviterra::Application.configure do
   # оверрайды - сюда
 #  config.action_mailer.delivery_method = :test
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_url_options = { :host => 'staging.eviterra.com' }
 end
