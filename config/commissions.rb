05b9ff44a9f9169970920feca898afe84f31f21e
inque <me@inque.ru>
2013-05-17 20:09:55 +0400

    fix AF,KL: повышенная комиссия для AFKL через dtt с 15.05.2013

--- config/commissions.rb
+++ config/commissions.rb
@@ -447,6 +447,19 @@ carrier "AF", "AIR FRANCE"
 
 carrier_defaults consolidator: 0, our_markup: '0.2%'
 
+example 'jfksvo/c svojfk/n'
+example 'jfksvo/v'
+strt_date "15.05.2013"
+agent "1232 DL/AFKL/AZ US-EMEAI Consolidator Commission Program Amendment #1"
+agent "Если, кратко, то C,D,Z,I W,S,Y,M,U,K,H A,L,Q,T,N,R,V"
+agent "Только перелеты в Америку из России и наоборот (RT и OW), только СОБСТВЕННЫЕ рейсы ( никаких код-шерингов), авиакомпании могут комбинироваться в одном бронировании. Их комиссия 8%, наша 6%, никаких особенностей в выписке"
+subagent "6%"
+subclasses "CDZIWSYMUKHALQTNRV"
+check { includes(country_iatas, 'RU') and includes(country_iatas.first, 'US') and includes_only(country_iatas, 'US RU') }
+ticketing_method "downtown"
+discount "5%"
+commission "8%/6%"
+
 example 'svocdg'
 example 'svocdg cdgsvo/ab'
 strt_date "01.07.2012"
@@ -1471,6 +1484,19 @@ carrier "KL", "KLM"
 ########################################
 
 carrier_defaults consolidator: 0, our_markup: '0.2%'
+
+example 'jfksvo/c svojfk/n'
+example 'jfksvo/v'
+strt_date "15.05.2013"
+agent "1232 DL/AFKL/AZ US-EMEAI Consolidator Commission Program Amendment #1"
+agent "Если, кратко, то C,D,Z,I W,S,Y,M,U,K,H A,L,Q,T,N,R,V"
+agent "Только перелеты в Америку из России и наоборот (RT и OW), только СОБСТВЕННЫЕ рейсы ( никаких код-шерингов), авиакомпании могут комбинироваться в одном бронировании. Их комиссия 8%, наша 6%, никаких особенностей в выписке"
+subagent "6%"
+subclasses "CDZIWSYMUKHALQTNRV"
+check { includes(country_iatas, 'RU') and includes(country_iatas.first, 'US') and includes_only(country_iatas, 'US RU') }
+ticketing_method "downtown"
+discount "5%"
+commission "8%/6%"
 
 example 'svocdg'
 agent    "1руб за билет, выписанный по опубл. тарифам, в случае перевозки с вылетом из стран СНГ; 1руб за билет, выписанный по опубл. тарифам,  в случае вылета вне стран СНГ;"
