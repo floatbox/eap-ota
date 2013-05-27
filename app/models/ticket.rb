90a34067202c8bcebccd960c6a5cdb11d1fd63f9
abragin <abragin@gmail.com>
2013-05-27 17:14:37 +0400

    Не пересчитываем price_acquiring_compensation у возвратов

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -75,7 +75,7 @@ class Ticket < ActiveRecord::Base
   before_save :set_prices
 
   def set_prices
-    self.price_acquiring_compensation = price_payment_commission if corrected_price
+    self.price_acquiring_compensation = price_payment_commission if corrected_price && kind == 'ticket'
     self.price_difference = price_with_payment_commission - price_real
   end
 
