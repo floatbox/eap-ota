19e5a7bddb3a4b104a5637478a36dfc14323c93f
codesnik <aronaxis@gmail.com>
2013-06-03 20:09:31 +0400

    lib/simple_flyweight.rb documentated

--- lib/simple_flyweight.rb
+++ lib/simple_flyweight.rb
@@ -1,10 +1,16 @@
-# очень, очень простое кэширование immutable объектов
-# кэш не очищается, бойтесь утечек!
+# encoding: utf-8
 #
-# Usage:
+# Очень, очень простое кэширование immutable объектов.
+# Кэш не очищается, бойтесь утечек!
+#
+# @example Usage
 #   class Foo
 #     extend SimpleFlyweight
+#     ...
 #   end
+#
+#   Foo.new(3).object_id == Foo.new(3).object_id
+#   => 3
 module SimpleFlyweight
   def new(*args)
     @pool ||= {}
