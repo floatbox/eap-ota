58c7de2684737706a67c83867aa77b8238bbdf9c
Denis Redozubov <denis.redozubov@gmail.com>
2013-06-03 21:02:37 +0400

    riemann connection doc

--- lib/monitoring/riemann_connection.rb
+++ lib/monitoring/riemann_connection.rb
@@ -12,6 +12,20 @@ module Monitoring
         @connection = Riemann::Client.new(host: Conf.riemann.host)
       end
 
+      # @!method meter
+      # @!method histogram
+      # @!method gauge
+      # @!method occurrence
+      # @!method timer
+      # @param [Hash] содержимое ивента
+      # @option opts [String] :service полное название метрики с точками или пробелами в качестве разделителя
+      # @option opts [Integer] :metric (0) количественная метрика ивента
+      # @option opts [String] :host (hostname машины) хост с которого отправлен ивент, как правило не нужно указывать явно
+      # @option opts [Array] :tags ([]) массив с тегами, используется для распознавания типа ивента в riemann
+      # отправляет в riemann ивент с тегом, одноименным методу(тег определяет вид графика в graphite)
+      # @example Отправляет в riemann значение, для отрисовки гистограммы
+      #   Monitoring.histogram(service: 'this.is.my.metric.', metric: 3.14)
+      #   @return [Boolean] возвращает true, если отправлено
       %w|meter histogram gauge occurrence timer|.each do |type|
         define_method :"#{type}", ->(event_hash={}) do
           event_hash[:tags] = [*event_hash[:tags]]
