6f688b19607a431da01030e97a07a24b23f678ce
Nick Zayarny <nickzayarny@gmail.com>
2013-06-18 15:37:52 +0400

    апдейт BT

--- config/commissions.rb
+++ config/commissions.rb
@@ -681,6 +681,7 @@ example 'svocdg cdgsvo/ab'
 agent "1 руб. с билета по всем опубл. тарифам на собств. рейсы BT и рейсы Interline с участком BT"
 subagent "50 КОП. с билета по всем опубл. тарифам на собств. рейсы BT и рейсы Interline с участком BT +2% сбор АЦ"
 interline :no, :yes
+discount "2.5%"
 commission "1/0.5"
 
 carrier "CA", "AIR CHINA"
