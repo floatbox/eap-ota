54b6bd65d5bd7b932ad3bbd37dd02d9777a47f4d
Andrey Arefiev <arefiev@gmail.com>
2013-06-20 21:07:19 +0400

    блок Трипстера на странице успешного бронирования

--- app/helpers/excursions_helper.rb
+++ app/helpers/excursions_helper.rb
@@ -6,8 +6,18 @@ module ExcursionsHelper
     weatlas_uri uri_params
   end
 
+  def tripster_link order_form
+    return unless MongoStorage.read('cities',:namespace=>'tripster').include?(order_form.recommendation.journey.segments.first.arrival.city.iata)
+    return unless uri_params = tripster_uri_params(order_form)
+    tripster_uri uri_params
+  end
+
   def weatlas_uri uri_params
     "http://weatlas.com/get.php?aid=10002&#{uri_params.to_query}"
+  end
+
+  def tripster_uri uri_params
+    "http://experience.tripster.ru/partner/v1/iata?partner=eviterra&content=v1&#{uri_params.to_query}"
   end
 
   def weatlas_uri_params order_form
@@ -18,8 +28,31 @@ module ExcursionsHelper
     }
   end
 
-  def weatlas_city order_form
+  def tripster_uri_params order_form
+    return unless order_form
+    return unless order_form.recommendation
+
+    journey = order_form.recommendation.journey
+    departure_date = journey.segments.first.arrv_date
+    return_date =
+      if journey.segments.count > 1
+        journey.segments.last.dept_date
+      else
+        departure_date + 7.days
+      end
+    {
+      'city' => order_form.recommendation.journey.segments.first.arrival.city.iata,
+      'departure' => tripster_date(departure_date),
+      'return' => tripster_date(return_date)
+    }
+  end
+
+  def city_case_in order_form
     order_form.recommendation.journey.segments.first.arrival.city.case_in
+  end
+
+  def tripster_date date
+    date.strftime('%d.%m.%Y')
   end
 
 end
