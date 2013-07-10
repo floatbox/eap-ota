db2a9aed0aeec5cf0b18cd20e19a3084ec59dbac
abragin <abragin@gmail.com>
2013-07-10 19:27:14 +0400

    Запускаем авто-тикетинг после 3ds

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -156,7 +156,7 @@ class BookingController < ApplicationController
       else
         strategy = Strategy.select(:order => @order)
 
-        unless strategy.delayed_ticketing?
+        if  !strategy.delayed_ticketing?
           logger.info "Pay: ticketing"
 
           unless strategy.ticket
@@ -164,6 +164,10 @@ class BookingController < ApplicationController
             logger.info "Pay: ticketing failed"
             @error_message = :ticketing
             @order.unblock!
+          end
+        else
+          if @order.ok_to_auto_ticket?
+            AutoTicketStuff.new(order: @order).create_auto_ticket_job
           end
         end
       end
