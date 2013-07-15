69f2ea5c41aa64ba1fb2ef204465a76454485dfe
Andrey Arefiev <arefiev@gmail.com>
2013-07-15 19:05:52 +0400

    подправил дата-миграцию для связи ордеров и кастомеров

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -222,10 +222,11 @@ module DataMigration
   end
 
   def self.fill_customers_for_orders
-    Order.where("customer_id IS NULL").limit(20).each do |order|
+    Order.where("customer_id IS NULL").limit(200).each do |order|
       customer_email = order.email.strip.split(/[,; ]/).first
-      order.customer = Customer.find_or_create_by_email(customer_email)
-      order.save
+      customer = Customer.find_or_create_by_email(customer_email)
+      order.update_column(:customer_id, customer.id)
+      #order.save
     end
   end
 
