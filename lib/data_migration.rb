015864260c36de05853becfee5ff14f970a5156e
Andrey Arefiev <arefiev@gmail.com>
2013-07-24 22:26:22 +0400

    поправил дата-миграцию для ЛК что бы она не посылала приглашения

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -237,7 +237,10 @@ module DataMigration
   def self.fill_customers_for_orders
     Order.where("customer_id IS NULL").limit(200).each do |order|
       customer_email = order.email.strip.split(/[,; ]/).first
-      customer = Customer.find_or_create_by_email(customer_email)
+      #customer = Customer.find_or_create_by_email(customer_email)
+      customer = Customer.find_or_initialize_by_email(customer_email)
+      customer.skip_confirmation_notification!
+      customer.save unless customer.persisted?
       order.update_column(:customer_id, customer.id)
       #order.save
     end
