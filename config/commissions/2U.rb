5afe3d17fd40ade5edcd31082bb59f11a152d405
codesnik <aronaxis@gmail.com>
2013-07-29 16:38:28 +0400

    смержил правило 2U: интерлайн не прописан

--- config/commissions/2U.rb
+++ config/commissions/2U.rb
@@ -1,16 +1,12 @@
 carrier "2U"
 
 example "svocdg"
-agent "5% от опубл. тарифов на собств. рейсы 2U (В договоре Interline отдельно не прописан.)"
-subagent "3,5% от опубл. тарифов на собств. рейсы 2U"
-discount "2.5%"
-ticketing_method "aviacenter"
-commission "5%/3.5%"
-
 example "cdgsvo svocdg/ab"
-agent "1р Interline не прописан"
-subagent "0р Interline не прописан"
-interline :unconfirmed
+agent "5% от опубл. тарифов на собств. рейсы 2U (В договоре Interline отдельно не прописан.)"
+agent "Interline не прописан"
+subagent "3,5% от опубл. тарифов на собств. рейсы 2U"
+subagent "Interline не прописан"
+interline :no, :unconfirmed
 discount "2.5%"
 ticketing_method "aviacenter"
 commission "5%/3.5%"
