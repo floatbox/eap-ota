9f69d6b6af4ce5b7a4b4a88d90049c43bd394790
Denis Redozubov <denis.redozubov@gmail.com>
2013-07-31 18:14:48 +0400

    спеки вслепую

--- spec/lib/code_stash_spec.rb
+++ spec/lib/code_stash_spec.rb
@@ -10,15 +10,37 @@ describe CodeStash do
 
   it "should not raise when searching for russian code" do
     pending "sirena is disabled for now"
-    expect { Airport['ДМД'] }.to_not raise_error(CodeStash::NotFound)
+    Airport['ДМД'].should_not raise_error(CodeStash::NotFound)
   end
 
-  it "should raise an exception trying to find unknown iata" do
-    expect { Airport['FFFF'] }.to raise_error(CodeStash::NotFound)
+  it "should raise an exception trying to find unknown airport by iata" do
+    Airport['ZZZ'].should raise_error(CodeStash::NotFound)
+    Airport.fetch_by_code('ZZZ').should raise_error(CodeStash::NotFound)
+  end
+
+  it "should raise an exception trying to find unknown carrier by iata" do
+    Carrier['ZZ'].should raise_error(CodeStash::NotFound)
+    Carrier.fetch_by_code('ZZ').should raise_error(CodeStash::NotFound)
+  end
+
+  it "should raise an exception trying to find unknown city by iata" do
+    City['ZZZ'].should raise_error(CodeStash::NotFound)
+    City.fetch_by_code('ZZZ').to_a.should raise_error(CodeStash::NotFound)
+  end
+
+  it "should raise an exception trying to find unknown country iata" do
+    Country['ZZ'].should raise_error(CodeStash::NotFound)
+    Country.fetch_by_code('ZZ').should raise_error(CodeStash::NotFound) 
   end
 
   it "exception trying to find unknown iata" do
     (Airport['FFFF'] rescue $!.message).should == "Couldn't find Airport with code 'FFFF'"
   end
+
+  it "should not raise an exception then searching for unknown airplane" do
+    Airplane['BBQ'].should_not raise_error(CodeStash::NotFound)
+    Airplane.where(iata: 'BBQ', auto_save: true).should_not be_nil
+  end
+
 end
 
