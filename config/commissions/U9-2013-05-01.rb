c89ea17bcea4fdecb35446df30b890b19defa8b5
codesnik <aronaxis@gmail.com>
2013-08-01 13:00:00 +0400

    U9: мы умеем определять code-share

--- config/commissions/U9-2013-05-01.rb
+++ config/commissions/U9-2013-05-01.rb
@@ -3,31 +3,30 @@ carrier "U9", start_date: "2013-05-01"
 example "svocdg/business cdgsvo/business"
 agent "4% от опубл. тарифов на собств. рейсы U9 Бизнес класса;"
 subagent "3% от опубл. тарифов на собств. рейсы U9 Бизнес класса;"
+interline :no_codeshare
 classes :business
 discount "3%"
 ticketing_method "aviacenter"
 commission "4%/3%"
 
-example "svocdg/economy svocdg/economy"
+example "svocdg svocdg"
+example "svocdg svocdg/business"
+comment "бизнес класс отфильтрован правилом #1, поэтому тут классы не указываем"
 agent "3% от опубл. тарифов на собств. рейсы U9 Эконом класса;"
 subagent "2% от опубл. тарифов на собств. рейсы U9 Эконом класса;"
-classes :economy
+interline :no_codeshare
 discount "2%"
 ticketing_method "aviacenter"
 commission "3%/2%"
 
 example "svocdg/ab cdgsvo"
+example "svocdg/ab:u9 cdgsvo"
+example "svocdg/ab:u9/business"
 agent "1% от всех опубл. тарифов на рейсы Interline;"
-subagent "5 руб от всех опубл. тарифов на рейсы Interline;"
-interline :yes
-consolidator "2%"
-ticketing_method "aviacenter"
-commission "1%/5"
-
 agent "1% от всех опубл. тарифов на рейсы code-share."
+subagent "5 руб от всех опубл. тарифов на рейсы Interline;"
 subagent "5 руб от всех опубл. тарифов на рейсы code-share;"
+interline :no, :yes
 consolidator "2%"
 ticketing_method "aviacenter"
-not_implemented "не умеем определять code-share"
 commission "1%/5"
-
