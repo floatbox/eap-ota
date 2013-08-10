b31b422886854c71a3f5de0d0b07bd9813949124
codesnik <aronaxis@gmail.com>
2013-08-11 00:00:24 +0400

    временная затычка для preliminary_booking

--- lib/commission/finder.rb
+++ lib/commission/finder.rb
@@ -6,11 +6,12 @@ class Commission::Finder
 
   def cheap!(rec, opts={})
     rules = @book.find_rules_for_rec(rec, opts).select(&:sellable?)
-    best_rule = rules.min_by do |rule|
-      # side effect!
-      rec.commission = rule
-      rec.price_total
-    end
+    #best_rule = rules.min_by do |rule|
+    #  # side effect!
+    #  rec.commission = rule
+    #  rec.price_total
+    #end
+    best_rule = rules.first
     rec.commission = best_rule || Commission::Rule::Null
   end
 
