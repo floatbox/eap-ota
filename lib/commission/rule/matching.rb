df5e6635b9923f8bf7557b39f28ee45d6f38d516
codesnik <aronaxis@gmail.com>
2013-08-13 21:46:43 +0400

    символы для валидации правил

--- lib/commission/rule/matching.rb
+++ lib/commission/rule/matching.rb
@@ -26,14 +26,14 @@ module Commission::Rule::Matching
   end
 
   def turndown_reason recommendation
-    skipped? and return "правило не проверяется"
+    skipped?                                and return :skipped     # "правило не проверяется"
     # carrier == recommendation.validating_carrier_iata and
-    applicable_interline?(recommendation) or return "интерлайн/не интерлайн"
-    applicable_classes?(recommendation) or return "не подходит класс бронирования"
-    applicable_subclasses?(recommendation) or return "не подходит подкласс бронирования"
-    applicable_custom_check?(recommendation) or return "не прошло дополнительную проверку"
-    applicable_routes?(recommendation) or return "маршрут не в списке применимых"
-    applicable_geo?(recommendation) or return "не тот тип МВЛ/ВВЛ"
+    applicable_interline?(recommendation)    or return :interline   # "интерлайн/не интерлайн"
+    applicable_classes?(recommendation)      or return :classes     # "не подходит класс бронирования"
+    applicable_subclasses?(recommendation)   or return :subclasses  # "не подходит подкласс бронирования"
+    applicable_custom_check?(recommendation) or return :check       # "не прошло дополнительную проверку"
+    applicable_routes?(recommendation)       or return :routes      # "маршрут не в списке применимых"
+    applicable_geo?(recommendation)          or return :geo         # "не тот тип МВЛ/ВВЛ"
     nil
   end
 
@@ -60,8 +60,6 @@ module Commission::Rule::Matching
       when :less_than_half
         recommendation.interline? and
         recommendation.validating_carrier_makes_more_than_half_of_itinerary?
-      else
-        raise ArgumentError, "неизвестный тип interline у #{carrier}: '#{interline}' (line #{source})"
       end
     end
   end
@@ -87,7 +85,6 @@ module Commission::Rule::Matching
 
   def applicable_geo? recommendation
     return true unless domestic || international
-    raise "wtf?" if domestic && international
     domestic && recommendation.domestic? || international && recommendation.international?
   end
 
