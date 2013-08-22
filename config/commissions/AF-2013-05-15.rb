76f7dd621545b84359dd92db3b7f4870e639e86f
codesnik <aronaxis@gmail.com>
2013-08-22 13:11:53 +0400

    уменьшаю скидки по дтт

--- config/commissions/AF-2013-05-15.rb
+++ config/commissions/AF-2013-05-15.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "downtown"
 agent "8%"
 subagent "6%"
-discount "7%"
+discount "3%"
 agent_comment "1232 DL/AFKL/AZ US-EMEAI Consolidator Commission Program Amendment #1"
 agent_comment "Если, кратко, то C,D,Z,I W,S,Y,M,U,K,H A,L,Q,T,N,R,V"
 agent_comment "Только перелеты в Америку из России и наоборот (RT и OW), только СОБСТВЕННЫЕ рейсы ( никаких код-шерингов), авиакомпании могут комбинироваться в одном бронировании. Их комиссия 8%, наша 6%, никаких особенностей в выписке"
