a383591662662d78626b49e5be0465115d512880
codesnik <aronaxis@gmail.com>
2013-08-26 12:06:33 +0400

    скидки по авиацентру

--- config/commissions/OM-2013-08-07.rb
+++ config/commissions/OM-2013-08-07.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "5%"
 subagent "2.5%"
-discount "2.5%"
+discount "1.25%"
 agent_comment "5% от всех опубл. тарифов на рейсы OM (В договоре Interline не прописан.)"
 subagent_comment "2.5% от всех опубл. тарифов на рейсы OM"
 interline :no, :unconfirmed
--- config/commissions/W5.rb
+++ config/commissions/W5.rb
@@ -5,7 +5,7 @@ disabled "starting from 13 OCT 2011 Mahan Air (W5/537) is suspended from BSP."
 ticketing_method "aviacenter"
 agent "5%"
 subagent "3%"
-discount "3%"
+discount "1.5%"
 agent_comment "5 % от всех опубликованных тарифов; (Interline отдельно не прописан)"
 subagent_comment "3% от всех опубл.тарифов на собств. рейсы W5"
 interline :no, :unconfirmed
