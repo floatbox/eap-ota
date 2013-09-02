24dfd732c918d9fbe5803c64200e392658c962a7
inque <me@inque.ru>
2013-09-02 20:53:59 +0400

    Небольшая проверка комиссий перед распилом по центрам выписки

--- config/commissions/AB-2013-07-01.rb
+++ config/commissions/AB-2013-07-01.rb
@@ -13,21 +13,6 @@ example "cdgfra/m fracdg/s"
 end
 
 rule 2 do
-disabled "пока dtt?"
-ticketing_method "aviacenter"
-agent "5%"
-subagent "3%"
-discount "3%"
-consolidator "2%"
-agent_comment "5% (3%) (3%) за все опубл. тарифы, включая групповые (комиссия не распространяется на:топливный сбор, сбор за безопасность и все остальные сборы, а также на дополнительные услуги)."
-agent_comment "Период бронирования и выписки: С 5 по 31.07.2013г (включительно)."
-agent_comment "Период путешествия: Любые даты, начиная с 5 июня 2013 г."
-agent_comment "Применяется только к рейсам, выполняемым airberlin group (AB/HG/4T). Билеты должны быть выписаны на бланках AB/745."
-check %{ includes_only(operating_carrier_iatas, 'AB HG 4T') }
-example "cdgfra/m fracdg/s"
-end
-
-rule 3 do
 ticketing_method "direct"
 agent "1"
 subagent "0.05"
@@ -39,7 +24,7 @@ example "cdgfra/S7:AB"
 example "cdgsvo svocdg/lh"
 end
 
-rule 4 do
+rule 3 do
 no_commission
 example "svocdg/s7"
 end
