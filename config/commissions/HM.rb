c77ad4e4c0be16be957023cdd828736e3797e7db
inque <me@inque.ru>
2013-09-23 19:01:51 +0400

    новые скидки

--- config/commissions/HM.rb
+++ config/commissions/HM.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "2%"
+discount "2.5%"
 agent_comment "1% от опубл. тарифа на собств. рейсы HM (В договоре Interline не прописан.)"
 subagent_comment "0,5% от опубл. тарифа на собств. рейсы HM"
 example "svocdg"
@@ -14,7 +14,7 @@ rule 2 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "2%"
+discount "2.5%"
 agent_comment "1р Interline не прописан"
 subagent_comment "0р Interline не прописан"
 interline :unconfirmed
