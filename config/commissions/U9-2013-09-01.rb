3ba895cd2e0312a772b9bab1540d4fffd7a83e37
inque <me@inque.ru>
2013-09-23 16:49:48 +0400

    fix U9: новые комиссии с начала сентября

--- config/commissions/U9-2013-09-01.rb
+++ config/commissions/U9-2013-09-01.rb
@@ -15,11 +15,12 @@ end
 rule 2 do
 ticketing_method "aviacenter"
 agent "1%"
-subagent "0.5%"
+subagent "5"
 discount "2%"
+consolidator "2%"
 comment "бизнес класс отфильтрован правилом #1, поэтому тут классы не указываем"
 agent_comment "1% от опубл. тарифов на собств. рейсы U9 Эконом класса;"
-subagent_comment "0.5% от опубл. тарифов на собств. рейсы U9 Эконом класса;"
+subagent_comment "5 р. от опубл. тарифов на собств. рейсы U9 Эконом класса;"
 interline :no_codeshare
 example "svocdg svocdg"
 example "svocdg svocdg/business"
