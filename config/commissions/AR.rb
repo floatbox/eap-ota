6d77d63fa27596158b9ad577cf45c678030d9e96
inque <me@inque.ru>
2013-09-24 16:45:02 +0400

    АЦ +0.25%, dtt +0.5% скидки

--- config/commissions/AR.rb
+++ config/commissions/AR.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "5"
-discount "3%"
+discount "3.25%"
 consolidator "2%"
 agent_comment "1% от всех опубл. Тарифов"
 subagent_comment "5 руб. с билета от всех опубл. тарифов"
