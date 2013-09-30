7b4aad54a06aa4aae85312a1614acfb5df6d220a
Denis Redozubov <denis.redozubov@gmail.com>
2013-09-30 23:18:25 +0400

    попытка завести капистрано

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@
 require "rvm/capistrano"
 require "capistrano_colors"
 
+set :default_shell, :bash
 set :rvm_type, :system
 # закрепил версию, чтобы не прыгала в продакшне
 set :rvm_ruby_string, 'ruby-1.9.3-p327'
