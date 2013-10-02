28a1eaed8acc1a832a8f7f6da8f585140c354040
abragin <abragin@gmail.com>
2013-10-02 20:22:40 +0400

    Strategy#fare_rule_hashes

--- lib/amadeus/request/fare_price_pnr_with_booking_class.rb
+++ lib/amadeus/request/fare_price_pnr_with_booking_class.rb
@@ -1,10 +1,11 @@
 module Amadeus
   module Request
     class FarePricePNRWithBookingClass < Amadeus::Request::Base
-      attr_accessor :validating_carrier, :unifares
+      attr_accessor :validating_carrier, :unifares, :request_options
 
       def initialize(*)
         @unifares = true
+        @request_options = ['ETK', 'RP']
         super
       end
 
