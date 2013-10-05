61dcf2b9fcc8bc4c25af5e430931887f5a88f269
inque <me@inque.ru>
2013-10-05 22:15:10 +0400

    Aviacenter +0.25%

--- config/commissions/9U.rb
+++ config/commissions/9U.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "5%"
 subagent "3%"
-discount "5.5%"
+discount "5.75%"
 agent_comment "5 (пять) % от всех опубликованных тарифов."
 subagent_comment "3% от опубл. тарифов на рейсы 9U"
 example "dmekiv"
--- config/commissions/OV.rb
+++ config/commissions/OV.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "3.5%"
+discount "3.75%"
 agent_comment "1% от всех опубл. тарифов на рейсы OV (В договоре Interline не прописан.)"
 subagent_comment "0,5% от опубл. тарифа на рейсы OV"
 interline :no, :unconfirmed
