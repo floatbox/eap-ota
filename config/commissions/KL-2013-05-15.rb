1a06674cbce40c7f2130ad9b784acbb6ffcccc49
inque <me@inque.ru>
2013-10-10 11:51:05 +0400

    Убрали выписку в dtt у KL и AF

--- config/commissions/KL-2013-05-15.rb
+++ config/commissions/KL-2013-05-15.rb
@@ -1,21 +1,6 @@
 carrier "KL", start_date: "2013-05-15"
 
 rule 1 do
-ticketing_method "downtown"
-agent "8%"
-subagent "6%"
-discount "6%"
-agent_comment "1232 DL/AFKL/AZ US-EMEAI Consolidator Commission Program Amendment #1"
-agent_comment "Если, кратко, то C,D,Z,I W,S,Y,M,U,K,H A,L,Q,T,N,R,V"
-agent_comment "Только перелеты в Америку из России и наоборот (RT и OW), только СОБСТВЕННЫЕ рейсы ( никаких код-шерингов), авиакомпании могут комбинироваться в одном бронировании. Их комиссия 8%, наша 6%, никаких особенностей в выписке"
-subagent_comment "6%"
-subclasses "CDZIWSYMUKHALQTNRV"
-routes "RU...US/ALL"
-example "jfksvo/c svojfk/n"
-example "jfksvo/v"
-end
-
-rule 2 do
 ticketing_method "aviacenter"
 agent "1"
 subagent "0.05"
