c2eb191105072857c9413551721cee31a88f0940
codesnik <aronaxis@gmail.com>
2013-10-11 16:58:44 +0400

    скидки по авиацентру +1%, по даунтауну - 50% от субагентской

--- config/commissions/WY.rb
+++ config/commissions/WY.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "5%"
 subagent "3%"
-discount "4%"
+discount "5%"
 agent_comment "5% от опубл. тарифов на собств. рейсы WY (В договоре Interline не прописан.)"
 subagent_comment "3% от опубл. тарифа на собств.рейсы WY"
 example "svocdg"
@@ -14,7 +14,7 @@ rule 2 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "1.35%"
+discount "2.35%"
 agent_comment "1р Interline не прописан"
 subagent_comment "0р Interline не прописан"
 interline :unconfirmed
