1e27a1534f1f7cef13997ee790b64e5ed8ab89b8
inque <me@inque.ru>
2013-10-17 19:26:48 +0400

    Новые скидки: 0.3 от вознаграждения для комиссионных и надбавка 150 для некомиссионных

--- config/commissions/IY.rb
+++ config/commissions/IY.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "1.5%"
+discount "0.15%"
 agent_comment "1% от опубл. тарифов на собств. рейсы IY (В договоре Interline отдельно не прописан.)"
 subagent_comment "0,5% от опубл. тарифов на собств. рейсы IY"
 example "svocdg"
@@ -14,7 +14,7 @@ rule 2 do
 ticketing_method "aviacenter"
 agent "1%"
 subagent "0.5%"
-discount "1.5%"
+discount "0.15%"
 agent_comment "1р Interline не прописан"
 subagent_comment "0р Interline не прописан"
 interline :unconfirmed
--- config/commissions/UT.rb
+++ config/commissions/UT.rb
@@ -4,7 +4,7 @@ rule 1 do
 ticketing_method "downtown"
 agent "5%"
 subagent "4%"
-discount "7%"
+discount "1.2%"
 agent_comment "5% DTT"
 subagent_comment "4% DTT"
 example "svocdg cdgsvo"
