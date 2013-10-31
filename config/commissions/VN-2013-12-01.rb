06336651b05749045b09e8fb0c4112bc5b13f349
inque <me@inque.ru>
2013-10-31 19:12:03 +0400

    Новый VN с первого декабря

--- config/commissions/VN-2013-12-01.rb
+++ config/commissions/VN-2013-12-01.rb
@@ -13,11 +13,11 @@ end
 
 rule 2 do
 ticketing_method "aviacenter"
-agent "3%"
-subagent "1%"
-discount "2%"
-agent_comment "3% от опубликованных тарифов VN на всех внутренних рейсах VN во Вьетнаме;"
-subagent_comment "2% от опубликованных тарифов VN на всех внутренних рейсах VN во Вьетнаме;"
+agent "1%"
+subagent "5"
+consolidator "2%"
+agent_comment "С 01.12.13г. 1% (5 руб+сбор АЦ) от опубл. тарифов VN на всех внутренних рейсах VN во Вьетнаме;"
+subagent_comment "С 01.12.13г. 1% (5 руб+сбор АЦ) от опубл. тарифов VN на всех внутренних рейсах VN во Вьетнаме;"
 domestic
 example "hansgn"
 end
