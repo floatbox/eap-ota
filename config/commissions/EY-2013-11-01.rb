eaff805a8bf8cfbbfcdd3508fc6f76f08260f0d6
inque <me@inque.ru>
2013-10-31 18:24:23 +0400

    Новый EY с 1 ноября

--- config/commissions/EY-2013-11-01.rb
+++ config/commissions/EY-2013-11-01.rb
@@ -1,6 +1,26 @@
 carrier "EY", start_date: "2013-11-01"
 
 rule 1 do
-no_commission "не заданы правила"
+agent "10%"
+subagent "8%"
+discount "9%"
+agent_comment "по 31.12.13г. 10% (8%) от опубл. тарифов Первого и Бизнес класса на собств.рейсы EY, включая код-шеринг сегменты, выписанные на бланках EY с сегментом EY. А также на сквозные рейсы Interline с обязательным участием EY."
+subagent ""
+classes :first, :business
+interline :no, :yes
+example "svocdg/first cdgsvo/first"
+example "svocdg/business"
+example "svocdg/ab/business cdgsvo/first"
+end
+
+rule 2 do
+agent "5%"
+subagent "3%"
+discount "4%"
+agent_comment "5% (3%) от опубл. тарифов Эконом класса на собств.рейсы EY, а также на сквозные рейсы Interline с обязательным участием EY."
+subagent ""
+interline :no, :yes
+example "svocdg"
+example "svocdg/ab cdgsvo"
 end
 
