3cac812b614ced3c555e5815c44ce30e3ba11057
codesnik <aronaxis@gmail.com>
2013-11-02 02:47:16 +0400

    забыли ticketing_method, от этого заказы не сохраняются

--- config/commissions/EY-2013-11-01.rb
+++ config/commissions/EY-2013-11-01.rb
@@ -1,6 +1,7 @@
 carrier "EY", start_date: "2013-11-01"
 
 rule 1 do
+ticketing_method "aviacenter"
 agent "10%"
 discount "9%"
 agent_comment "по 31.12.13г. 10% (8%) от опубл. тарифов Первого и Бизнес класса на собств.рейсы EY, включая код-шеринг сегменты, выписанные на бланках EY с сегментом EY. А также на сквозные рейсы Interline с обязательным участием EY."
@@ -12,6 +13,7 @@ example "svocdg/ab/business cdgsvo/first"
 end
 
 rule 2 do
+ticketing_method "aviacenter"
 agent "5%"
 discount "4%"
 agent_comment "5% (3%) от опубл. тарифов Эконом класса на собств.рейсы EY, а также на сквозные рейсы Interline с обязательным участием EY."
