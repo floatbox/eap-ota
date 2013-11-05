3259888fcc6bfb6907de4714dbadadace4089956
inque <inque@inques-MacBook-Pro.local>
2013-11-05 19:11:46 +0400

    PS: выписка через АЦ и новые сроки продажи

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -152,7 +152,7 @@ class Recommendation
       f.marketing_carrier_iata == 'PS' && (
         booking_class_for_flight(f) == 'T' ||
         # PS возможно закроется, избавляемся от новогодних возвратов
-        f.dept_date && f.dept_date > Date.new(2013, 12, 1)
+        f.dept_date && f.dept_date > Date.new(2013, 12, 31) && f.dept_date < Date.new(2014, 5, 1)
       )
     end
     commission.sellable?
