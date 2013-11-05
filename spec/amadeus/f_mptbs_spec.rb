3b3e6f7841bb9d7936adc02f6270751b489feecd
codesnik <aronaxis@gmail.com>
2013-11-06 00:55:59 +0400

    объединил спеки

--- spec/amadeus/f_mptbs_spec.rb
+++ spec/amadeus/f_mptbs_spec.rb
@@ -2,6 +2,18 @@
 require 'spec_helper'
 
 describe Amadeus::Response::FareMasterPricerTravelBoardSearch, :amadeus do
+  context 'simple case' do
+
+    let_once! :response do
+      amadeus_response('spec/amadeus/xml/Fare_MasterPricerTravelBoardSearch_simple_case.xml')
+    end
+
+    subject { response }
+    it { should be_success }
+    it { should have(19).recommendations }
+    specify {response.recommendations.first.last_tkt_date.should == Date.parse("12JUN12")}
+  end
+
   context 'with NO ITINERARY FOUND' do
 
     let_once! :response do
