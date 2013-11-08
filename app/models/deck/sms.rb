7c84605e4080b6df8e6200a53bff613c814f5ce3
Denis Redozubov <denis.redozubov@gmail.com>
2013-11-08 23:29:48 +0400

    валидатор длины sms сообщений для разных character set-ов

--- app/models/deck/sms.rb
+++ app/models/deck/sms.rb
@@ -1,9 +1,10 @@
+# encoding: utf-8
+
 class Deck::SMS < ActiveRecord::Base
   set_table_name :deck_sms
   attr_accessible :status, :message, :address
 
   validate :status, presence: true
   validate :address, presence: true
-  # TODO добавить смс-валидатор
-  validate :message#, sms: true
+  validate :message, presence: true, allow_blank: false, sms: true
 end
--- lib/sms.rb
+++ lib/sms.rb
@@ -3,21 +3,10 @@
 DEFAULT = SMS::MFMS
 
 module SMS
-  class Message
-    def valid?(message_body)
-      # длина смс с кириллицей - 70 символов
-      # без - 160 символов
-      max_length = message_body =~ /\P{ASCII}/ ? 70 : 160
-      message_body.length <= max_length
-    end
-
-  end
-
   def gate(*args)
     return @gate if args && @gate_args == args
     @gate_args = args
     @gate = DEFAULT.new(*args)
   end
-
 end
 
