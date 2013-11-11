7598c5fe3d394341dd2e5f320bdadf6e00364a84
Denis Redozubov <denis.redozubov@gmail.com>
2013-11-11 18:25:19 +0400

    добавил акроним в инфлектор

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,6 +6,7 @@
 ActiveSupport::Inflector.inflections do |inflect|
 # TODO попробовать включить, должно повлиять на классы типа PNRMailer
     inflect.acronym 'PNR'
+    inflect.acronym 'HTTP'
 #
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
