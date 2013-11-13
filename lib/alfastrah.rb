55963a13ee1bb795c4077a875ace2d3e3fbb867e
Max Barnash <i.am@anhero.ru>
2013-11-13 13:41:50 +0400

    Базовые методы для запросов в апи Альфастраха

--- lib/alfastrah.rb
+++ lib/alfastrah.rb
@@ -1,8 +1,35 @@
 module Alfastrah
+  URL_BASE = 'uat2.vtsft.ru'
+  API_BASE = '/travel-ext-services/TravelExtService'
+
   PRODUCTS = {
     flight: 'EVITERRA-FLIGHT',
     travel: 'EVITERRA-TRAVEL'
   }
 
-  ENDPOINT = 'https://uat2.vtsft.ru/travel-ext-services/TravelExtService'
+  def self.calculate *args
+    request = Alfastrah::Calculation::Request.new *args
+    response = post request.endpoint, request.build_xml
+    Alfastrah::Calculation::Response.new response.body
+  end
+
+  def self.purchase *args
+    request = Alfastrah::Purchase::Request.new *args
+    response = post request.endpoint, request.build_xml
+    response = Alfastrah::Purchase::Response.new response.body
+
+    request = Alfastrah::Confirmation::Request.new policy_id: response.policy_id
+    response = post request.endpoint, request.build_xml
+    Alfastrah::Confirmation::Response.new response.body
+  end
+
+  private
+
+  def self.post endpoint, body
+    transport = Net::HTTP.new(URL_BASE, 443)
+    transport.use_ssl = true
+    transport.ssl_version = :SSLv3
+    transport.verify_mode = OpenSSL::SSL::VERIFY_NONE # нужно выключить, когда они починят сертификат
+    transport.request_post [API_BASE, endpoint].join('/'), body
+  end
 end
