47e6f02878060aebe08219ab8ebab04bff7768f9
Andrey Arefiev <arefiev@gmail.com>
2013-11-14 19:09:17 +0400

    таки почИнил spyglass

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,15 +1,19 @@
 class ProfileController < ApplicationController
 
-  before_filter :authenticate_customer!, :except=> [:spyglass]
+  before_filter :authenticate_customer!, :except=> [:spyglass, :orders]
   before_filter :check_order_permission, :only => [:show_pnr, :itinerary]
 
   def orders
     if admin_user && params[:current_customer_id]
       current_customer = Customer.find(params[:current_customer_id])
     end
-    @orders = current_customer.orders.profile_orders
-    @exchanged_tickets_numbers = current_customer.orders.profile_orders.collect {|o| o.profile_exchanged_tickets_numbers}.flatten || []
-    render :partial => 'orders'
+    if current_customer
+      @orders = current_customer.orders.profile_orders
+      @exchanged_tickets_numbers = current_customer.orders.profile_orders.collect {|o| o.profile_exchanged_tickets_numbers}.flatten || []
+      render :partial => 'orders'
+    else
+      redirect_to :profile
+    end
   end
 
   # FIXME обязательно убрать этот метод при выкладке на прод
