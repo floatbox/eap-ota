08cb5d0a2d83f25f39091d00a10331e4bec29758
Max Barnash <i.am@anhero.ru>
2013-11-14 13:53:43 +0400

    Немного отрефакторил Alfastrah
    * Вынес запрос-ответ в отдельный метод
    * Перенос ошибок из запросов в базовый класс

--- lib/alfastrah.rb
+++ lib/alfastrah.rb
@@ -1,5 +1,5 @@
 class Alfastrah
-  attr_accessor :host, :base_url
+  attr_reader :host, :base_url, :errors
 
   PRODUCTS = {
     flight: 'EVITERRA-FLIGHT',
@@ -11,29 +11,38 @@ class Alfastrah
     @base_url = options.fetch :base_url, Conf.alfastrah.base_url
   end
 
-  def calculate *args
-    request = Alfastrah::Calculation::Request.new *args
-    response = post request.endpoint, request.build_xml
-    Alfastrah::Calculation::Response.new response.body
+  def calculate params
+    get_response Alfastrah::Calculation, params
   end
 
-  def purchase *args
-    request = Alfastrah::Purchase::Request.new *args
-    response = post request.endpoint, request.build_xml
-    response = Alfastrah::Purchase::Response.new response.body
+  def purchase params
+    purchase_response = get_response Alfastrah::Purchase, params
+    return unless purchase_response
 
-    request = Alfastrah::Confirmation::Request.new policy_id: response.policy_id
-    response = post request.endpoint, request.build_xml
-    Alfastrah::Confirmation::Response.new response.body
+    get_response Alfastrah::Confirmation, policy_id: purchase_response.policy_id
   end
 
   private
 
-  def post endpoint, body
+  def get_response action, params
+    @errors  = nil
+    request  = action.const_get('Request', false).new params
+    response = post request
+    response = action.const_get('Response', false).new response.body
+
+    if response.success?
+      response
+    else
+      @errors = response.errors
+      false
+    end
+  end
+
+  def post request
     transport = Net::HTTP.new(host, 443)
     transport.use_ssl = true
     transport.ssl_version = :SSLv3
     transport.verify_mode = OpenSSL::SSL::VERIFY_NONE # нужно выключить, когда они починят сертификат
-    transport.request_post [base_url, endpoint].join('/'), body
+    transport.request_post [base_url, request.endpoint].join('/'), request.build_xml
   end
 end
