ba57e9cc1e1b2924ecf3f23505ee76646e3f087d
codesnik <aronaxis@gmail.com>
2013-11-19 03:12:43 +0400

    all green. Yay!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,6 +55,9 @@ Spork.prefork do
       DeferredGarbageCollection.reconsider
     end
     # /deferred GC
+
+    # https://github.com/plataformatec/devise/wiki/How-To:-Controllers-tests-with-Rails-3-(and-rspec)
+    config.include Devise::TestHelpers, :type => :controller
   end
 
 end
