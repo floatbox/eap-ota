c2fff210fdb63e2f4463514c57381e6c0b785e8b
Max Barnash <i.am@anhero.ru>
2013-11-18 18:38:15 +0400

    Вернул двойной to_s,  because XPATH

--- lib/amadeus/response/fare_check_rules.rb
+++ lib/amadeus/response/fare_check_rules.rb
@@ -14,7 +14,7 @@ module Amadeus
           to = fd.xpath('r:odiGrp/r:originDestination/r:destination').to_s
           passenger_type = fd.xpath('r:qualificationFareDetails/r:fareDetails/r:qualifier').to_s
           rule_text = fd.xpath('r:travellerGrp/r:fareRulesDetails/r:ruleSectionId').map do |rule_section_id|
-            xpath("//r:tariffInfo[r:fareRuleInfo/r:ruleSectionLocalId='#{rule_section_id}']/r:fareRuleText/r:freeText").map{|t| t.to_s}.join("\n")
+            xpath("//r:tariffInfo[r:fareRuleInfo/r:ruleSectionLocalId='#{rule_section_id}']/r:fareRuleText/r:freeText").map{|t| t.to_s.to_s}.join("\n")
           end.join("\n")
           {carrier: marketing_carrier, fare_base: fare_base, from_iata: from, to_iata: to, passenger_type: passenger_type, rule_text: rule_text}
         end.select{|rh| rh[:carrier].present?}
