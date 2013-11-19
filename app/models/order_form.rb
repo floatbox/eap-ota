bdcc8bb2fd77ce930bc1627d17e5cc2c92baf74a
codesnik <aronaxis@gmail.com>
2013-11-19 19:28:07 +0400

    на всякий пожарный случай

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -133,7 +133,7 @@ class OrderForm
         day: pa.delete('document_expiration_day'),
         month: pa.delete('document_expiration_month'),
         year: pa.delete('document_expiration_year')
-      } if pa['document_expiration_day']
+      } if pa['document_expiration_day'] && pa['document_noexpiration'] == '0'
       end
 
       @people[k.to_i] = Person.new(pa)
