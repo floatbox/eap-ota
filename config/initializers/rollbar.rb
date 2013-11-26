49abd37d4108d9d7cb1a65b7e3d4dadebce7eb30
Max Barnash <i.am@anhero.ru>
2013-11-26 18:00:23 +0400

    Заткнул роутинг эрроры

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,9 +2,11 @@ require 'rollbar/rails'
 Rollbar.configure do |config|
   config.access_token = 'b810757b1a234503a1611a223d097ad4'
 
-  if Rails.env.test? #|| Rails.env.development?
+  if Rails.env.test? || Rails.env.development?
     config.enabled = false
   end
+
+  config.exception_level_filters.merge!('ActionController::RoutingError' => 'ignore')
 
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`,
