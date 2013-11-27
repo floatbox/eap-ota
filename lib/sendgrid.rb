457e627f77520f2d551f676036fb1045cac38e2d
Max Barnash <i.am@anhero.ru>
2013-11-27 22:05:40 +0400

    Базовый клиент для получения баунсов от сендгрида

---
+++ lib/sendgrid.rb
@@ -0,0 +1,33 @@
+class Sendgrid
+  AUTH_BODY = {
+    api_user: Conf.sendgrid.username,
+    api_key: Conf.sendgrid.password
+  }
+
+  attr_accessor :host, :base_url
+
+  def initialize options = {}
+    @host     = options.fetch :host,     Conf.sendgrid.host
+    @base_url = options.fetch :base_url, Conf.sendgrid.base_url
+  end
+
+  def bounces options = {}
+    body = options.slice(:email, :date_start, :date_end)
+
+    response = request('bounces.get.json', body).body
+    response = MultiJson.load response
+
+    response
+  end
+
+  private
+
+  def request endpoint, options = {}
+    return '' unless Conf.sendgrid.enabled
+    transport = Net::HTTP.new(host, 443)
+    transport.use_ssl = true
+
+    body = AUTH_BODY.merge(options).to_param
+    transport.request_post [base_url, endpoint].join('/'), body
+  end
+end
