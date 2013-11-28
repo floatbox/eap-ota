7ab5be6e6decbcb4a7d8a94dfdb57ab45d1b20ec
Denis Redozubov <denis.redozubov@gmail.com>
2013-11-28 20:55:49 +0400

    простенькая обработка параметров в RapidaController

--- app/controllers/rapida_controller.rb
+++ app/controllers/rapida_controller.rb
@@ -2,8 +2,25 @@
 
 class RapidaController < ApplicationController
   def payment
-    # заглушка, позже убью
-    render text: 'foo'
+    get_params!
+    rapida = Rapida.new
+    response = case @command
+    when 'check' then rapida.check(@txn_id, @account, @sum)
+    when 'pay' then rapida.pay(@txn_id, @account, @sum, @phone)
+    else return render :status => :not_acceptable, :text => 'command not allowed'
+    end
+    render text: response
   end
+
+  private
+
+  def get_params!
+    @command = params[:command]
+    @txn_id = params[:txn_id]
+    @account = params[:account]
+    @sum = params[:sum]
+    @phone = params[:phone]
+  end
+
 end
 
