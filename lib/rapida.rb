f748cda62d30804d822efa35e3031341d36d496f
Denis Redozubov <denis.redozubov@gmail.com>
2013-11-28 20:55:27 +0400

    Сервис(пока пустой) для обработки рапиды

---
+++ lib/rapida.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+class Rapida
+  # класс для работы с RapidaCharge и всем-всем-всем
+
+  def check(txn_id, account, sum)
+    # заглушка
+    'check'
+  end
+
+  def pay(txn_id, account, sum, phone)
+    # заглушка
+    'pay'
+  end
+
+  def error
+  end
+
+  def fatal_error
+  end
+end
+
