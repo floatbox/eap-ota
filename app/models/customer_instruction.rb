36da9ff7732b9e4de15e5acf98a83d294996ca27
Andrey Arefiev <arefiev@gmail.com>
2013-11-28 18:14:00 +0400

    умный статус в админке писем

--- app/models/customer_instruction.rb
+++ app/models/customer_instruction.rb
@@ -2,4 +2,9 @@ class CustomerInstruction < ActiveRecord::Base
   belongs_to :customer
 
   def self.statuses; ["sent", "error", "delayed"] end
+
+  def sent_status
+      (status + ' to ' + email).html_safe
+  end
+
 end
