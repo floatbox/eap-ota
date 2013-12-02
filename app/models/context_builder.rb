0a094be7cfb7430d52af9c7701f4bf8a6c1ab26d
Max Barnash <i.am@anhero.ru>
2013-12-02 17:21:05 +0400

    Убрал изменение контекста после создания

--- app/models/context_builder.rb
+++ app/models/context_builder.rb
@@ -3,19 +3,23 @@ class ContextBuilder
     @options = options
     @options[:partner] = Partner.anonymous
     @options[:robot] = false
+    @frozen = false
   end
 
   def build!
+    @frozen = true
     context = Context.new @options.slice(:partner, :robot, :deck_user)
     Rails.logger.info "Initialized context: #{context.inspect}"
     context
   end
 
   def robot= value
+    raise if @frozen
     @options[:robot] = value
   end
 
   def partner= partner_or_code
+    raise if @frozen
     @options[:partner] = case partner_or_code
     when Partner
       partner_or_code
@@ -26,6 +30,7 @@ class ContextBuilder
   end
 
   def deck_user= user
+    raise if @frozen
     @options[:deck_user] = user
   end
 end
