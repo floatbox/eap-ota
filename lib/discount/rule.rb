4c232188967c53dd3e19dd85cd7f19ceb2d97e4b
Max Barnash <i.am@anhero.ru>
2013-12-03 18:19:18 +0400

    Понятный inspect для скидок

--- lib/discount/rule.rb
+++ lib/discount/rule.rb
@@ -15,23 +15,31 @@ class Discount::Rule
   # @return [Commission::Formula]
   has_commission_attrs :discount
 
+  attr_accessor :source, :meta
+
   def initialize(*)
     @discount = Fx(0)
     @our_markup = Fx(0)
+    @source ||= self.class.caller_address
+    @meta ||= 'direct'
     super
   end
 
   def self.netto(commission, discount)
     new(
       discount: (commission.subagent.extract('%') + Commission::Formula.new(discount)).round(2),
-      our_markup: '0'
+      our_markup: '0',
+      source: caller_address,
+      meta: "netto discount #{discount.inspect} of #{commission.subagent.extract('%').inspect}"
     )
   end
 
   def self.scaled(commission, multiplier)
     new(
       discount: (commission.subagent.extract('%') * multiplier).round(2),
-      our_markup: '0'
+      our_markup: '0',
+      source: caller_address,
+      meta: "scaled discount #{multiplier} * #{commission.subagent.extract('%').inspect}"
     )
   end
 
@@ -48,6 +56,17 @@ class Discount::Rule
       @discount = Fx(0)
       @our_markup = commission
     end
+  end
+
+  def inspect
+    "<Discount::Rule discount: #{discount.inspect} markup: #{our_markup.inspect} at #{source} (#{meta})"
+  end
+
+  def self.caller_address level=1
+    caller[level] =~ /([a-zA-Z0-9-]+\.rb:\d+)/
+    # по каким-то причинам тут приходит US-ASCII
+    # конвертирую для yaml
+    ($1 || 'unknown').encode('UTF-8')
   end
 
 end
