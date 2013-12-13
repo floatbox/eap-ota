69f656abaa24ea8a47e43ba93a656e605b74c35f
Max Barnash <i.am@anhero.ru>
2013-12-13 15:07:34 +0400

    Новые скидки: 5.5%

--- config/discounts/all.rb
+++ config/discounts/all.rb
@@ -1,7 +1,25 @@
 # для app-ios 500 маркап
 # для прямого половину от комиссии на комиссионные и маркап 250 р. на некомиссионные
+# для остальных скидки 5.5% от нетто
+Discount.register "2013-12-13 16:00" do
+  case context.partner_code
+  when 'anonymous'
+    if commission.subagent.extract('%') > Fx(0)
+      Discount::Rule.scaled(commission, 0.5)
+    else
+      Discount::Rule.new(our_markup: 250)
+    end
+  when 'app-ios'
+    Discount::Rule.new(our_markup: 500)
+  else
+    Discount::Rule.netto(commission, '5.5%')
+  end
+end
+
+# для app-ios 500 маркап
+# для прямого половину от комиссии на комиссионные и маркап 250 р. на некомиссионные
 # для остальных скидки 5% от нетто
-Discount.register "2013-12-12" do
+Discount.register "2013-12-13" do
   case context.partner_code
   when 'anonymous'
     if commission.subagent.extract('%') > Fx(0)
