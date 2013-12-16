4d3de4e749145c680f9e075a96c33dcaf98c3cb6
Denis Redozubov <denis.redozubov@gmail.com>
2013-12-16 19:33:20 +0400

    отдельный префикс для рефандов рапиды

--- app/models/rapida_refund.rb
+++ app/models/rapida_refund.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 
 class RapidaRefund < Payment
+
+  # TODO в Payment поднять пора такую штуку
+  def generate_ref
+    Conf.rapida.ref_refund_prefix + id.to_s if persisted?
+  end
+
 end
 
