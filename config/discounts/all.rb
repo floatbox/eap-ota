2e5dc960de275592e849682c94e4aa735522b130
Max Barnash <i.am@anhero.ru>
2013-12-16 12:32:46 +0400

    Новые скидки: АЦ +0.5%

--- config/discounts/all.rb
+++ config/discounts/all.rb
@@ -1,5 +1,29 @@
 # для app-ios 500 маркап
 # для прямого половину от комиссии на комиссионные и маркап 250 р. на некомиссионные
+# для АЦ скидка 6% от нетто
+# для DTT скидка 5.5% от нетто
+Discount.register "2013-12-16 12:42" do
+  case context.partner_code
+  when 'anonymous'
+    if commission.subagent.extract('%') > Fx(0)
+      Discount::Rule.scaled(commission, 0.5)
+    else
+      Discount::Rule.new(our_markup: 250)
+    end
+  when 'app-ios'
+    Discount::Rule.new(our_markup: 500)
+  else
+    case commission.ticketing_method
+    when 'aviacenter'
+      Discount::Rule.netto(commission, '6%')
+    else
+      Discount::Rule.netto(commission, '5.5%')
+    end
+  end
+end
+
+# для app-ios 500 маркап
+# для прямого половину от комиссии на комиссионные и маркап 250 р. на некомиссионные
 # для остальных скидки 5.5% от нетто
 Discount.register "2013-12-13 16:00" do
   case context.partner_code
