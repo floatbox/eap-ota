883ff04f76b53fac528a24140b940396c179b6f8
inque <me@inque.ru>
2013-12-24 17:11:05 +0400

    Фикс SU с 1 января

--- config/commissions/SU-2014-01-01.rb
+++ config/commissions/SU-2014-01-01.rb
@@ -3,30 +3,22 @@ carrier "SU", start_date: "2014-01-01"
 rule 1 do
 ticketing_method "aviacenter"
 agent "0.1%"
+subagent "5"
+consolidator "2%"
 agent_comment "С 01.01.2014г. 0,1% от тарифа на собств. рейсы SU с началом перевозки из РФ (вкл. рейсы по согл. «Код-шеринг» и рейсы Interline с участком SU, а также по субсидированным перевозкам);"
 subagent_comment "30 okt 2013 — субагентской пока нет"
-interline :no, :yes
-routes "RU..."
+interline :no, :yes, :absent
 example "svotlv/su tlvsvo"
-end
-
-rule 2 do
-important!
-ticketing_method "aviacenter"
-agent "1eur"
-consolidator "2%"
-agent_comment "1 евро с билета на собств. рейсы SU с началом перевозки за пределами РФ (вкл. рейсы по согл. «Код-    шеринг» и рейсы Interline с участком SU, а также по субсидированным перевозкам);"
-interline :yes
-check %{ not includes_only(country_iatas.first, 'RU') }
 example "cdgsvo/ab svocdg/su"
+example "cdgsvo/ab"
 end
 
 rule 3 do
 ticketing_method "aviacenter"
-agent "1eur"
+disabled "не умеем"
+agent "1"
+subagent "0.05"
 consolidator "2%"
-agent_comment "1 евро с билета на рейсы Interline без участка SU, а также по тарифам: туроператорским,               конфиденциальным, 'нетто'."
-interline :absent
-example "cdgsvo/ab"
+agent_comment "С 01.01.2014г. 1 евро с билета при продаже под кодом SU по групповым, договорным, туроператорским и конфиденциальным тарифам. "
 end
 
