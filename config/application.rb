9d56932838945a894c78934b8377dbe1fe2768db
Max Barnash <i.am@anhero.ru>
2013-12-25 21:21:31 +0400

    Убрал ворнинг

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Eviterra
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    I18n.config.enforce_available_locales = false
     config.i18n.default_locale = :ru
 
     # Configure the default encoding used in templates for Ruby 1.9.
