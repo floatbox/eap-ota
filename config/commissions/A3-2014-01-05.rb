61444ded9f09d4f3cfd374c8f9adb1ba2bb3b3d7
inque <me@inque.ru>
2013-12-27 14:28:13 +0400

    Новый A3 с 5 января

--- config/commissions/A3-2014-01-05.rb
+++ config/commissions/A3-2014-01-05.rb
@@ -1,11 +1,11 @@
-carrier "A3", start_date: "2014-01-01"
+carrier "A3", start_date: "2014-01-05"
 
 rule 1 do
 ticketing_method "aviacenter"
-agent "9%"
-subagent "7%"
-agent_comment "9% для тарифов Бизнес класса"
-subagent_comment "7% для тарифов Бизнес класса"
+agent "7%"
+subagent "5%"
+agent_comment "7% для тарифов Бизнес класса"
+subagent_comment "5% для тарифов Бизнес класса"
 classes :business
 international
 example "svocdg/business cdgsvo/business"
@@ -13,10 +13,10 @@ end
 
 rule 2 do
 ticketing_method "aviacenter"
-agent "7%"
-subagent "5%"
-agent_comment " 7% для тарифов Экономического класса"
-subagent_comment "5% для тарифов Эконом класса"
+agent "4%"
+subagent "2%"
+agent_comment "4% для тарифов Экономического класса"
+subagent_comment "2% для тарифов Эконом класса"
 international
 example "scocdg cdgsvo"
 end
@@ -36,7 +36,19 @@ end
 rule 4 do
 ticketing_method "aviacenter"
 agent "1"
-subagent "0"
+subagent "0.05"
+consolidator "2%"
+agent_comment "1 рубль (0,05 руб+2% сбор АЦ) с тарифов по Р, G классам, IT- тарифы, а также авиабилеты, выписанные по интерлайн соглашению"
+subagent_comment "1 рубль (0,05 руб+2% сбор АЦ) с тарифов по Р, G классам, IT- тарифы, а также авиабилеты, выписанные по интерлайн соглашению"
+subclasses "PG"
+interline :yes
+example "svocdg/p cdgsvo/g/ab"
+end
+
+rule 5 do
+ticketing_method "aviacenter"
+agent "1"
+subagent "0.05"
 consolidator "2%"
 agent_comment "Билеты по интерлайн соглашению могут быть выписаны только при условии наличия сегментов Авиакопании."
 subagent_comment "Билеты по интерлайн соглашению могут быть выписаны только при условии наличия сегментов Авиакопании."
